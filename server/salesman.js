381ee685e3c7959d394ba74e5dc3384fdeef78e9
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-06 15:34:25 +0700

    Update salesman.js

--- server/salesman.js
+++ server/salesman.js
@@ -1,5 +1,19 @@
 var db = require('./pghelper');
 
+exports.createSalesman = function(req, res, next) {
+  if (!req.body) return res.sendStatus(400);
+  var query = "INSERT INTO salesforce.Salesman__c ( sfid, Name, IMEI__c, Area_Code__c, Code__c, Email__c, Phone__c ) VALUES ('";
+  query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.imei + "', '" + req.body.areacode + "', '" + req.body.code + "', '";
+  query += req.body.email + "', '" + req.body.phone + "')";
+  console.log(query);
+	
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "create salesman success" }');
+	})
+	.catch(next);
+}
+
 exports.getInfo = function(req, res, next) {
   var deviceId = req.headers['deviceid'];
   db.select("SELECT * FROM salesforce.Salesman__c WHERE IMEI__c ='" + deviceId + "'")
