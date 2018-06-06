2062b7d8e6c9f8085b1decfc04ffa9526189bdb3
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-06 09:48:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/userinfo', account.getInfo);
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
