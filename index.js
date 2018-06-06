6047b0edbe631e4daf3147be211b0a2085bd6da4
PolymorphGit <info.polymorph@gmail.com>
2018-06-06 09:29:47 +0700

    init

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var express = require('express')
 var path = require('path')
 var app = express()
-
+/*
 var account = require('./server/account')
 
-//var PORT = process.env.PORT || 5000
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
-})
+});
 
 app.get('/userinfo', account.getInfo);
+*/
