f1165189fcaaec7ac087d1ca7cb4384e8c0598ad
PolymorphGit <info.polymorph@gmail.com>
2018-06-06 09:20:34 +0700

    initial 3

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express')
+var path = require('path')
 var app = express()
 
+var account = require('./server/account')
+
+//var PORT = process.env.PORT || 5000
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
+
+app.get('/userinfo', account.getInfo);
