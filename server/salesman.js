8c90483838ff089f34081f342b3eab53b805857f
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-07 10:53:59 +0700

    Update salesman.js

--- server/salesman.js
+++ server/salesman.js
@@ -12,7 +12,7 @@ exports.createSalesman = function(req, res, next) {
 		res.send('{ \"status\": "create salesman success" }');
 	})
 	.catch(next);
-}
+};
 
 exports.getInfo = function(req, res, next) {
   var deviceId = req.headers['deviceid'];
@@ -22,4 +22,25 @@ exports.getInfo = function(req, res, next) {
     res.json(results);
   })
   .catch(next);
-}
+};
+
+exports.updateAccount = function(req, res, next) {
+	var id = req.params.id;
+	//console.log(id);
+	if (!req.body) return res.sendStatus(400);
+	var query = "UPDATE salesforce.Salesman__c SET ";
+	query += "Name = '" + req.body.name + "', ";
+	query += "IMEI__c = '" + req.body.imei + "', ";
+	query += "Area_Code__c = '" + req.body.areacode + "', ";
+	query += "Code__c = '" + req.body.code + "', ";
+	query += "Email__c = '" + req.body.email + "', ";
+	query += "Phone__c = '" + req.body.phone + "', ";
+	query += "WHERE sfid = '" + id + "'";
+	console.log(query);
+	
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "update success" }');
+	})
+	.catch(next);
+};
