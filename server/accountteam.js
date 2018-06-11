d666c398420e3def41ce2b35bbf6fa9dc32fe533
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-11 10:06:54 +0700

    Create accountteam.js

---
+++ server/accountteam.js
@@ -0,0 +1,28 @@
+var db = require('./pghelper');
+
+exports.createAccountTeam = function(req, res, next) {
+  if (!req.body) return res.sendStatus(400);
+  
+  var query = "INSERT INTO salesforce.account_team__c ( sfid, account__c, salesman__c ) VALUES ('";
+  query += req.body.sfid + "', '" + req.body.account + "', '" + req.body.sales + "')";
+  console.log(query);
+	
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "create account team success" }');
+	})
+	.catch(next);
+};
+
+exports.deleteAccountTeam = function(req, res, next) {
+  var id = req.params.id;
+	if (!req.body) return res.sendStatus(400);
+	var query = "DELETE FROM salesforce.account_team__c WHERE sfid = '" + id + "'";	
+	console.log(query);
+	
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "delete account team success" }');
+	})
+	.catch(next);
+};
