6cbfe54174de37cc16520c00afbcfbae9eed4015
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-12 14:08:25 +0700

    Update contact.js

--- server/contact.js
+++ server/contact.js
@@ -3,3 +3,11 @@ var db = require('./pghelper');
 exports.createContact = function(req, res, next) {
 
 };
+
+exports.updateContact = function(req, res, next) {
+
+};
+
+exports.deleteContact = function(req, res, next) {
+
+};
