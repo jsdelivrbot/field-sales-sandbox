001203ed6a562e65361c48dc227b5d4e53a267d8
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-12 18:43:46 +0700

    Create scaleprice.js

---
+++ server/scaleprice.js
@@ -0,0 +1,25 @@
+var db = require('./pghelper');
+
+exports.createSalesPrice = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+  
+};
+
+exports.updateSalesPrice = function(req, res, next) {
+	var id = req.params.id;
+	if (!req.body) return res.sendStatus(400);
+  
+};
+
+exports.deleteSalesPrice = function(req, res, next) {
+	var id = req.params.id;
+  //var query = "DELETE FROM salesforce.sales_price__c WHERE sfid = '" + id + "'";	
+	var query = "UPDATE salesforce.sales_price__c SET IsDeleted = true WHERE sfid ='" + id + "'"; 
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
