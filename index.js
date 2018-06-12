8e24162d2205c5226deeed4af87c25fb9caca47e
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-12 14:36:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var jsonParser = bodyParser.json()
 var salesman = require('./server/salesman')
 var account = require('./server/account')
 var accountTeam = require('./server/accountteam')
+var contact = require('./server/contact')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -22,14 +23,18 @@ app.post('/updatesalesman/:id', jsonParser, salesman.updateSalesman);
 app.get('/deletesalesman/:id', salesman.deleteSalesman);
 app.get('/userinfo/:id', salesman.getInfo);
 
-app.get('/accountlist', account.getList);
 app.post('/createaccount', jsonParser, account.createAccount);
-app.get('/accountinfo/:id', account.getInfo);
 app.post('/updateaccount/:id', jsonParser, account.updateAccount);
 app.get('/deleteaccount/:id', account.deleteAccount);
+app.get('/accountlist', account.getList);
+app.get('/accountinfo/:id', account.getInfo);
 
 app.post('/createaccountteam', jsonParser, accountTeam.createAccountTeam);
 app.get('/deleteaccountteam/:id', accountTeam.deleteAccountTeam);
+
+app.post('/createcontact', jsonParser, contact.createContact);
+app.post('/updatecontact/:id', jsonParser, contact.updateContact);
+app.get('/deletecontact/:id', contact.deleteContact);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
