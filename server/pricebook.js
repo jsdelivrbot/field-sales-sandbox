3fea6c61a118bd566ba84c7b1a53c00a212de85b
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-13 09:58:38 +0700

    Update pricebook.js

--- server/pricebook.js
+++ server/pricebook.js
@@ -3,10 +3,10 @@ var db = require('./pghelper');
 exports.createPricebook = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
 	
-	var query = "INSERT INTO salesforce.pricebook2 ( sfid, Name, Description, IsActive, ";
+	var query = "INSERT INTO salesforce.pricebook2 ( sfid, Name, Description, IsActive, createddate, systemmodstamp, ";
 	query += "IsDeleted ) VALUES ('";
 	query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.description + "', '";
-	query += req.body.active + "', false)";
+	query += req.body.active + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
 	console.log(query);
 
 	db.select(query)
@@ -24,6 +24,7 @@ exports.updatePricebook = function(req, res, next) {
 	query += "Name = '" + req.body.name + "', ";
 	query += "Description = '" + req.body.description + "', ";
 	query += "IsActive = '" + req.body.active + "', ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 	query += "Isdeleted = '" + req.body.isdeleted +"' ";
 	query += "WHERE sfid = '" + id + "'";
 	console.log(query);
@@ -38,7 +39,7 @@ exports.updatePricebook = function(req, res, next) {
 exports.deletePricebook = function(req, res, next) {
 	var id = req.params.id;
   	//var query = "DELETE FROM salesforce.pricebook2 WHERE sfid = '" + id + "'";	
-	var query = "UPDATE salesforce.pricebook2 SET IsDeleted = true WHERE sfid ='" + id + "'"; 
+	var query = "UPDATE salesforce.pricebook2 SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
 	console.log(query);
 
 	db.select(query)
