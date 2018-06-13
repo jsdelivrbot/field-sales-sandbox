bce4f2be770f11205862f1ecb3a3ac9c987e6b7f
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-13 10:06:05 +0700

    Update topstoreprogram.js

--- server/topstoreprogram.js
+++ server/topstoreprogram.js
@@ -4,9 +4,9 @@ exports.createTopStore = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
 
 	var query = "INSERT INTO salesforce.Top_Store_Program__c ( sfid, Name, Account__c, Date__c, Event_Type__c, ";
-	query += "IsDeleted ) VALUES ('";
+	query += "createddate, systemmodstamp, IsDeleted ) VALUES ('";
 	query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.account + "', '" + req.body.date + "', '";
-	query += req.body.type + "', false)";
+	query += req.body.type + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
 	console.log(query);
 
 	db.select(query)
@@ -25,6 +25,7 @@ exports.updateTopStore = function(req, res, next) {
 	query += "Account__c = '" + req.body.account + "', ";
 	query += "Date__c = '" + req.body.date + "', ";
 	query += "Event_Type__c = '" + req.body.type + "', ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 	query += "Isdeleted = '" + req.body.isdeleted +"' ";
 	query += "WHERE sfid = '" + id + "'";
 	console.log(query);
@@ -39,7 +40,7 @@ exports.updateTopStore = function(req, res, next) {
 exports.deleteTopStore = function(req, res, next) {
 	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.Top_Store_Program__c WHERE sfid = '" + id + "'";	
-	var query = "UPDATE salesforce.Top_Store_Program__c SET IsDeleted = true WHERE sfid ='" + id + "'"; 
+	var query = "UPDATE salesforce.Top_Store_Program__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
 	console.log(query);
 
 	db.select(query)
