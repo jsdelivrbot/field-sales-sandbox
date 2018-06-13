8ec00a094576e7417d2d7a003bf2d375ec50f5b4
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-13 09:59:34 +0700

    Update pricebookentry.js

--- server/pricebookentry.js
+++ server/pricebookentry.js
@@ -3,10 +3,10 @@ var db = require('./pghelper');
 exports.createPricebookentry = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
 
-	var query = "INSERT INTO salesforce.pricebook_entry__c ( sfid, Name, Product__c, Price_Book__c, ";
+	var query = "INSERT INTO salesforce.pricebook_entry__c ( sfid, Name, Product__c, Price_Book__c, createddate, systemmodstamp, ";
 	query += "IsDeleted ) VALUES ('";
 	query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.product + "', '";
-	query += req.body.pricebook + "', false)";
+	query += req.body.pricebook + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
 	console.log(query);
 
 	db.select(query)
@@ -24,6 +24,7 @@ exports.updatePricebookentry = function(req, res, next) {
 	query += "Name = '" + req.body.name + "', ";
 	query += "Product__c = '" + req.body.account + "', ";
 	query += "Price_Book__c = '" + req.body.date + "', ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 	query += "Isdeleted = '" + req.body.isdeleted +"' ";
 	query += "WHERE sfid = '" + id + "'";
 	console.log(query);
@@ -38,7 +39,7 @@ exports.updatePricebookentry = function(req, res, next) {
 exports.deletePricebookentry = function(req, res, next) {
 	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.pricebook_entry__c WHERE sfid = '" + id + "'";	
-	var query = "UPDATE salesforce.pricebook_entry__c SET IsDeleted = true WHERE sfid ='" + id + "'"; 
+	var query = "UPDATE salesforce.pricebook_entry__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
 	console.log(query);
 
 	db.select(query)
