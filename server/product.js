ba9a0a5569146a92bf051a6c1475802b6f643495
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-13 10:00:43 +0700

    Update product.js

--- server/product.js
+++ server/product.js
@@ -8,7 +8,7 @@ exports.createProduct = function(req, res, next) {
 	query += "Dimension_Length_CM__c, Dimension_Width_CM__c, FDA__c, Family, Gross_Weight_KG__c, Halal__c, ";
 	query += "Multipack__c, Net_Weight_G__c, Pack_Height_CM__c, Pack_Length_CM__c, Pack_Size__c, Pack_Weight_KG__c, ";
 	query += "Pack_Width_CM__c, ProductCode, Product_Group__c, Product_Image__c, QuantityUnitOfMeasure, ";
-	query += "Shelf_Life__c, Shelf_Stall__c, Size_in_Grams__c, StockKeepingUnit, ";
+	query += "Shelf_Life__c, Shelf_Stall__c, Size_in_Grams__c, StockKeepingUnit, createddate, systemmodstamp, ";
 	query += "IsDeleted ) VALUES ('";
 	query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.nameth + "', '" + req.body.barcode + "', '";
 	query += req.body.cortoncode + "', '" + req.body.canheight + "', '" + req.body.canwidth + "', '" + req.body.cartonweight + "', '";
@@ -16,7 +16,7 @@ exports.createProduct = function(req, res, next) {
 	query += req.body.packheight + "', '" + req.body.packlenght + "', '" + req.body.packsize + "', '" + req.body.packweight + "', '";
 	query += req.body.packwidth + "', '" + req.body.code + "', '" + req.body.group + "', '" + req.body.image + "', '";
 	query += req.body.unit + "', '" + req.body.shelflife + "', '" + req.body.shelfstall + "', '" + req.body.sizegrams + "', '";
-	query += req.body.sku + "', false)";
+	query += req.body.sku + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
 	console.log(query);
 
 	db.select(query)
@@ -61,6 +61,7 @@ exports.updateProduct = function(req, res, next) {
 	query += "Pack_Width_CM__c = '" + req.body.packwidth + "', ";
 	query += "Shelf_Life__c = '" + req.body.shelflife + "', ";
 	query += "Shelf_Stall__c = '" + req.body.shelfstall + "', ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 	query += "Isdeleted = '" + req.body.isdeleted +"' ";
 	query += "WHERE sfid = '" + id + "'";
 	console.log(query);
@@ -75,7 +76,7 @@ exports.updateProduct = function(req, res, next) {
 exports.deleteProduct = function(req, res, next) {
 	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.Product2 WHERE sfid = '" + id + "'";	
-	var query = "UPDATE salesforce.Product2 SET IsDeleted = true WHERE sfid ='" + id + "'"; 
+	var query = "UPDATE salesforce.Product2 SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
 	console.log(query);
 
 	db.select(query)
