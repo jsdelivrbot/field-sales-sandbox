4759384036bdf2a752c58e1cd73426644fa8aa10
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-13 09:54:28 +0700

    Update accountteam.js

--- server/accountteam.js
+++ server/accountteam.js
@@ -3,8 +3,9 @@ var db = require('./pghelper');
 exports.createAccountTeam = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
 
-	var query = "INSERT INTO salesforce.account_team__c ( sfid, account__c, salesman__c ) VALUES ('";
-	query += req.body.sfid + "', '" + req.body.account + "', '" + req.body.sales + "')";
+	var query = "INSERT INTO salesforce.account_team__c ( sfid, account__c, salesman__c, createddate, systemmodstamp, ";
+	query += "IsDeleted ) VALUES ('";
+	query += req.body.sfid + "', '" + req.body.account + "', '" + req.body.sales + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
 	console.log(query);
 
 	db.select(query)
@@ -17,7 +18,7 @@ exports.createAccountTeam = function(req, res, next) {
 exports.deleteAccountTeam = function(req, res, next) {
  	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.account_team__c WHERE sfid = '" + id + "'";	
-	var query = "UPDATE salesforce.account_team__c SET IsDeleted = true WHERE sfid = '" + id + "'";
+	var query = "UPDATE salesforce.account_team__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid = '" + id + "'";
 	console.log(query);
 
 	db.select(query)
