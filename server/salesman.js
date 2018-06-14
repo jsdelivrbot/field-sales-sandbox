4807e7dabb31f8e98fe6a036e5408c97c722e332
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-14 17:12:25 +0700

    Update salesman.js

--- server/salesman.js
+++ server/salesman.js
@@ -2,57 +2,58 @@ var db = require('./pghelper');
 
 exports.createSalesman = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
-	var query = "INSERT INTO salesforce.Salesman__c ( sfid, Name, IMEI__c, Area_Code__c, Code__c, Email__c, Phone__c, ";
-	query += "createddate, systemmodstamp, ";
-	query += "IsDeleted ) VALUES ('";
-	query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.imei + "', '";
-	query += req.body.areacode + "', '" + req.body.code + "', '" + req.body.email + "', '" + req.body.phone;
-	query += "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
-	console.log(query);
-	
-	db.select(query)
-	.then(function(results) {
-		//Create User on Auth0
-		var https = require('https');
-		var postBody = JSON.stringify({      
-			'client_id':'Ko42sNQ96ngSP1KTvs6FScGHPXThIwn6',
-			'username':req.body.email,
-			'email':req.body.email,
-			'password':'1234',
-			'connection':'Username-Password-Authentication'
-		});
-		var options = {
-			host: 'app98692077.auth0.com',
-			path: '/dbconnections/signup',
-			port: '443',
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json',
-				'Content-Length': Buffer.byteLength(postBody)
-			}
-		};
-		
-		callback = function(results) {
-			var str = '';
-			results.on('data', function(chunk) {
-				str += chunk;
-			});
-			results.on('end', function() {
-				try {
-					var obj = JSON.parse(str);
-					res.send('{ \"status\": "success", \"id\": obj._id }');
-				}
-				catch(ex) { res.status(887).send("{ \"status\": \"fail\" }"); }
-			});
+
+	//Create User on Auth0
+	var https = require('https');
+	var postBody = JSON.stringify({      
+		'client_id':'Ko42sNQ96ngSP1KTvs6FScGHPXThIwn6',
+		'username':req.body.email,
+		'email':req.body.email,
+		'password':'1234',
+		'connection':'Username-Password-Authentication'
+	});
+	var options = {
+		host: 'app98692077.auth0.com',
+		path: '/dbconnections/signup',
+		port: '443',
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(postBody)
 		}
-		var httprequest = https.request(options, callback);
-		httprequest.on('error', (e) => {
-		//console.log(`problem with request: ${e.message}`);
-			res.send('problem with request: ${e.message}');
+	};
+
+	callback = function(results) {
+		var str = '';
+		results.on('data', function(chunk) {
+			str += chunk;
 		});
-		httprequest.write(postBody);
-		httprequest.end();
-	})
-	.catch( function(error) {res.send(error);} );
+		results.on('end', function() {
+			try {
+				var obj = JSON.parse(str);
+				var query = "INSERT INTO salesforce.Salesman__c ( sfid, Name, IMEI__c, Area_Code__c, Code__c, Email__c, ";
+				query += "Phone__c, User_Id__c createddate, systemmodstamp, ";
+				query += "IsDeleted ) VALUES ('";
+				query += req.body.sfid + "', '" + req.body.name + "', '" + req.body.imei + "', '";
+				query += req.body.areacode + "', '" + req.body.code + "', '" + req.body.email + "', '";
+				query += obj._id + "', '" + req.body.phone;
+				query += "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
+				console.log(query);
+
+				db.select(query)
+				.then(function(results) {
+					res.send('{ \"status\": "success", \"id\": obj._id }');
+				}).catch( function(error) {res.send(error);} );
+			}
+			catch(ex) { res.status(887).send("{ \"status\": \"fail\" }"); }
+		});
+	}
+	var httprequest = https.request(options, callback);
+	httprequest.on('error', (e) => {
+	//console.log(`problem with request: ${e.message}`);
+		res.send('problem with request: ${e.message}');
+	});
+	httprequest.write(postBody);
+	httprequest.end();	
 };
 
 exports.updateSalesman = function(req, res, next) {
