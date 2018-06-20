66620785ac8dbb7facc862d8600304fadabd3c36
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-20 11:21:07 +0700

    Update promotion.js

--- server/promotion.js
+++ server/promotion.js
@@ -52,6 +52,61 @@ exports.deletePromotion = function(req, res, next) {
 };
 
 exports.getList = function(req, res, next) {
+	var head = req.headers['authorization'];
+	var limit = req.headers['limit'];
+	var start = req.headers['start'];
 	
-	res.send("Promotion");	
+	var https = require('https');
+	var options = {
+		host: 'app98692077.auth0.com',
+		path: '/userinfo',
+		port: '443',
+		method: 'GET',
+		headers: { 'authorization': head }
+	};
+
+	callback = function(results) {
+		var str = '';
+		results.on('data', function(chunk) {
+			str += chunk;
+		});
+		results.on('end', function() {
+			try {
+				console.log(str);
+				var obj = JSON.parse(str);
+				var query = "SELECT * FROM salesforce.promotion__c";
+				console.log(query);
+				db.select(query)
+				.then(function(results) {
+					var output = '[';
+					for(var i = 0 ; i < results.length ; i++)
+					{
+						output += '{"sfid":"' + results[i].sfid;
+						output += '", "Name":"' + results[i].name;
+						output += '", "Dtart":"' + results[i].start_date__c;
+						output += '", "End":"' + results[i].end_date__c;
+						output += '", "Description":"' + results[i].description__c;
+						output += '", "Url":"' + results[i].url__c;
+						output += '", "IsDeleted":' + results[i].isdeleted;
+						output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
+					}
+					if(results.length)
+					{
+						output = output.substr(0, output.length - 1);
+					}
+					output += ']';
+					console.log(output);
+					res.json(JSON.parse(output));
+				}) 
+				.catch(next); 	
+			}
+			catch(ex) { res.status(887).send("{ \"status\": \"fail\" }"); }
+		});
+	}
+			
+	var httprequest = https.request(options, callback);
+	httprequest.on('error', (e) => {
+		res.send('problem with request: ${e.message}');
+	});
+	httprequest.end();	
 };
