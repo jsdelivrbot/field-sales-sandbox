b2364681785564f65e7aec65b1125542bb364793
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-25 17:04:53 +0700

    Update account.js

--- server/account.js
+++ server/account.js
@@ -5,7 +5,7 @@ exports.getList = function(req, res, next) {
 	var limit = req.headers['limit'];
 	var start = req.headers['start'];
 	var startdate = req.headers['start-date'];
-	console.log(startdate);
+	
 	var https = require('https');
 	var options = {
 		host: 'app98692077.auth0.com',
@@ -14,7 +14,7 @@ exports.getList = function(req, res, next) {
 		method: 'GET',
 		headers: { 'authorization': head }
 	};
-
+	console.log(!isNaN(startdate));
 	callback = function(results) {
 		var str = '';
 		results.on('data', function(chunk) {
