e2edfd12ba20e224e31c4f52d9602c45776c8076
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-25 17:24:52 +0700

    Update account.js

--- server/account.js
+++ server/account.js
@@ -43,137 +43,140 @@ exports.getList = function(req, res, next) {
 				console.log(query);
 				db.select(query)
 				.then(function(results) {
-					var accountList = "(";
-					for(var i = 0 ; i < results.length ; i++)
+					if(results.length > 0)
 					{
-						accountList += "'" + results[i].sfid + "', ";
-					}
-					accountList = accountList.substr(0, accountList.length - 2);
-					accountList += ")";
-					
-					var query2 = "SELECT * FROM salesforce.Contact WHERE accountId IN " + accountList;
-					console.log(query2);
-					db.select(query2)
-					.then(function(results2) {
-						var query3 = "SELECT * FROM salesforce.Top_Store_Program__c WHERE account__c IN " + accountList;
-						console.log(query3);
-						db.select(query3)
-						.then(function(results3) {
-							var query4 = "SELECT * FROM salesforce.Product_History__c WHERE account__c IN " + accountList;
-							console.log(query4);
-							db.select(query4)
-							.then(function(results4) {
-								
-								var output = '[';
-								for(var i = 0 ; i < results.length ; i++)
-								{
-									output += '{"sfid":"' + results[i].sfid;
-									output += '", "Name":"' + results[i].name + ' ' + results[i].account_name_2__c;
-									output += ' ' + results[i].account_name_3__c + ' ' + results[i].account_name_4__c;
-									output += '", "AccountNumber":"' + results[i].accountnumber;
-									output += '", "Tax":"' + results[i].tax_number__c;
-									output += '", "Phone":"' + results[i].phone;
-									output += '", "Fax":"' + results[i].fax +'#' + results[i].fax_ext__c;
-									output += '", "Credit":"' + results[i].credit_limit__c;
-									output += '", "Address":"' + results[i].address_no__c + ' ' + results[i].billingstreet;
-									output += ' ' + results[i].billingcity + ' ' + results[i].billingcountry;
-									output += ' ' + results[i].billingpostalcode + ' ' + results[i].billingstate;
-									output += '", "Lat":"' + results[i].billinglatitude;
-									output += '", "Long":"' + results[i].billinglongitude;
-									output += '", "Pricebook":"' + results[i].price_book__c;
-									output += '", "Industry":"' + results[i].industry_name__c;
-									output += '", "SubIndustry":"' + results[i].industry_code_name__c;
-									output += '", "PaymentTerm":"' + results[i].payment_term_name__c;
-									output += '", "Region":"' + results[i].region_name__c;
-									output += '", "SalesDistrict":"' + results[i].sales_district_name__c;
-									//Contact
-									var contact = '[';
-									for(var j = 0 ; j < results2.length ; j++)
-									{
-										if(results2[j].accountid == results[i].sfid)
-										{
-											contact += '{"sfid":"' + results2[j].sfid;
-											contact += '", "Title":"' + results2[j].title;
-											contact += '", "Name":"' + results2[j].name;
-											contact += '", "Nickname":"' + results2[j].nickname__c;
-											contact += '", "Department":"' + results2[j].department;
-											contact += '", "Phone":"' + results2[j].phone;
-											contact += '", "Fax":"' + results2[j].fax;
-											contact += '", "Email":"' + results2[j].email;
-											contact += '", "Birthdate":"' + results2[j].birthdate;
-											contact += '", "Street":"' + results2[j].mailingstreet;
-											contact += '", "City":"' + results2[j].mailingcity;
-											contact += '", "Country":"' + results2[j].mailingcountry;
-											contact += '", "PostalCode":"' + results2[j].mailingpsotalcode;
-											contact += '", "Stage":"' + results2[j].mailingstage;
-											contact += '", "IsDeleted":' + results2[j].isdeleted;
-											contact += ', "systemmodstamp":"' + results2[j].systemmodstamp + '"},';
-										}
-									}
-									if(contact.length > 1)
-									{
-										contact = contact.substr(0, contact.length - 1);
-									}
-									contact += ']';
-									output += '", "Contact":' + contact;
-									//Top Store Program
-									var program = '[';
-									for(var j = 0 ; j < results3.length ; j++)
-									{
-										if(results3[j].account__c == results[i].sfid)
-										{
-											program += '{"sfid":"' + results3[j].sfid;
-											program += '", "Name":"' + results3[j].name;
-											program += '", "Date":"' + results3[j].date__c;
-											program += '", "Type":"' + results3[j].event_type__c;
-											program += '", "IsDeleted":' + results3[j].isdeleted;
-											program += ', "systemmodstamp":"' + results3[j].systemmodstamp + '"},';
-										}
-									}
-									if(program.length > 1)
-									{
-										program = program.substr(0, program.length - 1);
-									}
-									program += ']';
-									output += ', "Program":' + program;
-									//Product History
-									var history = '[';
-									for(var j = 0 ; j < results4.length ; j++)
-									{
-										if(results4[j].account__c == results[i].sfid)
-										{
-											history += '{"sfid":"' + results4[j].sfid;
-											history += '", "Product":"' + results4[j].product__c;
-											history += '", "IsDeleted":' + results4[j].isdeleted;
-											history += ', "systemmodstamp":"' + results4[j].systemmodstamp + '"},';
-										}
-									}
-									if(history.length > 1)
-									{
-										history = history.substr(0, history.length - 1);	
-									}
-									history += ']';
-									output += ', "History":' + history;
-									output += ', "IsDeleted":' + results[i].isdeleted;
-									output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
-								}
-								if(results.length)
-								{
-									output = output.substr(0, output.length - 1);
-								}
-								output += ']';
-								console.log(output);
-								res.json(JSON.parse(output));
+						var accountList = "(";
+						for(var i = 0 ; i < results.length ; i++)
+						{
+							accountList += "'" + results[i].sfid + "', ";
+						}
+						accountList = accountList.substr(0, accountList.length - 2);
+						accountList += ")";
 
-								//res.send(JSON.stringify(results2));
-								//res.send(JSON.stringify(results3));
-								//res.send(JSON.stringify(results4));
+						var query2 = "SELECT * FROM salesforce.Contact WHERE accountId IN " + accountList;
+						console.log(query2);
+						db.select(query2)
+						.then(function(results2) {
+							var query3 = "SELECT * FROM salesforce.Top_Store_Program__c WHERE account__c IN " + accountList;
+							console.log(query3);
+							db.select(query3)
+							.then(function(results3) {
+								var query4 = "SELECT * FROM salesforce.Product_History__c WHERE account__c IN " + accountList;
+								console.log(query4);
+								db.select(query4)
+								.then(function(results4) {
+
+									var output = '[';
+									for(var i = 0 ; i < results.length ; i++)
+									{
+										output += '{"sfid":"' + results[i].sfid;
+										output += '", "Name":"' + results[i].name + ' ' + results[i].account_name_2__c;
+										output += ' ' + results[i].account_name_3__c + ' ' + results[i].account_name_4__c;
+										output += '", "AccountNumber":"' + results[i].accountnumber;
+										output += '", "Tax":"' + results[i].tax_number__c;
+										output += '", "Phone":"' + results[i].phone;
+										output += '", "Fax":"' + results[i].fax +'#' + results[i].fax_ext__c;
+										output += '", "Credit":"' + results[i].credit_limit__c;
+										output += '", "Address":"' + results[i].address_no__c + ' ' + results[i].billingstreet;
+										output += ' ' + results[i].billingcity + ' ' + results[i].billingcountry;
+										output += ' ' + results[i].billingpostalcode + ' ' + results[i].billingstate;
+										output += '", "Lat":"' + results[i].billinglatitude;
+										output += '", "Long":"' + results[i].billinglongitude;
+										output += '", "Pricebook":"' + results[i].price_book__c;
+										output += '", "Industry":"' + results[i].industry_name__c;
+										output += '", "SubIndustry":"' + results[i].industry_code_name__c;
+										output += '", "PaymentTerm":"' + results[i].payment_term_name__c;
+										output += '", "Region":"' + results[i].region_name__c;
+										output += '", "SalesDistrict":"' + results[i].sales_district_name__c;
+										//Contact
+										var contact = '[';
+										for(var j = 0 ; j < results2.length ; j++)
+										{
+											if(results2[j].accountid == results[i].sfid)
+											{
+												contact += '{"sfid":"' + results2[j].sfid;
+												contact += '", "Title":"' + results2[j].title;
+												contact += '", "Name":"' + results2[j].name;
+												contact += '", "Nickname":"' + results2[j].nickname__c;
+												contact += '", "Department":"' + results2[j].department;
+												contact += '", "Phone":"' + results2[j].phone;
+												contact += '", "Fax":"' + results2[j].fax;
+												contact += '", "Email":"' + results2[j].email;
+												contact += '", "Birthdate":"' + results2[j].birthdate;
+												contact += '", "Street":"' + results2[j].mailingstreet;
+												contact += '", "City":"' + results2[j].mailingcity;
+												contact += '", "Country":"' + results2[j].mailingcountry;
+												contact += '", "PostalCode":"' + results2[j].mailingpsotalcode;
+												contact += '", "Stage":"' + results2[j].mailingstage;
+												contact += '", "IsDeleted":' + results2[j].isdeleted;
+												contact += ', "systemmodstamp":"' + results2[j].systemmodstamp + '"},';
+											}
+										}
+										if(contact.length > 1)
+										{
+											contact = contact.substr(0, contact.length - 1);
+										}
+										contact += ']';
+										output += '", "Contact":' + contact;
+										//Top Store Program
+										var program = '[';
+										for(var j = 0 ; j < results3.length ; j++)
+										{
+											if(results3[j].account__c == results[i].sfid)
+											{
+												program += '{"sfid":"' + results3[j].sfid;
+												program += '", "Name":"' + results3[j].name;
+												program += '", "Date":"' + results3[j].date__c;
+												program += '", "Type":"' + results3[j].event_type__c;
+												program += '", "IsDeleted":' + results3[j].isdeleted;
+												program += ', "systemmodstamp":"' + results3[j].systemmodstamp + '"},';
+											}
+										}
+										if(program.length > 1)
+										{
+											program = program.substr(0, program.length - 1);
+										}
+										program += ']';
+										output += ', "Program":' + program;
+										//Product History
+										var history = '[';
+										for(var j = 0 ; j < results4.length ; j++)
+										{
+											if(results4[j].account__c == results[i].sfid)
+											{
+												history += '{"sfid":"' + results4[j].sfid;
+												history += '", "Product":"' + results4[j].product__c;
+												history += '", "IsDeleted":' + results4[j].isdeleted;
+												history += ', "systemmodstamp":"' + results4[j].systemmodstamp + '"},';
+											}
+										}
+										if(history.length > 1)
+										{
+											history = history.substr(0, history.length - 1);	
+										}
+										history += ']';
+										output += ', "History":' + history;
+										output += ', "IsDeleted":' + results[i].isdeleted;
+										output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
+									}
+									if(results.length)
+									{
+										output = output.substr(0, output.length - 1);
+									}
+									output += ']';
+									console.log(output);
+									res.json(JSON.parse(output));
+
+									//res.send(JSON.stringify(results2));
+									//res.send(JSON.stringify(results3));
+									//res.send(JSON.stringify(results4));
+								}) 
+								.catch(next); 
 							}) 
-							.catch(next); 
+							.catch(next);  
 						}) 
-						.catch(next);  
-					}) 
-					.catch(next);  
+						.catch(next);
+					}
 				})
 				.catch(next);      
 			}
