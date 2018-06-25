ebc4156e4f810899dc3ae1dbce1691360172e120
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-25 10:46:01 +0700

    Update order.js

--- server/order.js
+++ server/order.js
@@ -5,7 +5,7 @@ exports.getList = function(req, res, next) {
 	var limit = req.headers['limit'];
 	var start = req.headers['start'];
   
-  var https = require('https');
+  	var https = require('https');
 	var options = {
 		host: 'app98692077.auth0.com',
 		path: '/userinfo',
@@ -23,12 +23,56 @@ exports.getList = function(req, res, next) {
 			try {
 				console.log(str);
 				var obj = JSON.parse(str);
-				var query = "SELECT * FROM salesforce.promotion__c";
+				var sales = obj.nickname;
+				var query = "SELECT * FROM salesforce.order WHERE LOWER(salesman__c) = '" + sales + "' ) Order by delivery_date__c asc";
+				if(!isNaN(limit) && limit > 0)
+				{
+					query += " limit " + limit;
+				}
+				if(!isNaN(start) && start > 0)
+				{
+					query += " OFFSET  " + start;
+				}
 				console.log(query);
 				db.select(query)
 				.then(function(results) {
-        
-        }) 
+					var orderList = "(";
+					for(var i = 0 ; i < results.length ; i++)
+					{
+						orderList += "'" + results[i].sfid + "', ";
+					}
+					orderList = orderList.substr(0, orderList.length - 2);
+					orderList += ")";
+					
+					var query2 = "SELECT * FROM salesforce.orderitem WHERE orderId IN " + orderList;
+					console.log(query2);
+					db.select(query2)
+					.then(function(results2) {
+						var output = '[';
+						for(var i = 0 ; i < results.length ; i++)
+						{
+							output += '{"sfid":"' + results[i].sfid;
+							output += '", "AccountId":"' + results[i].accountid;
+							output += '", "CallVisit":"' + results[i].call_visit__c;
+							output += '", "DeliveryDate":"' + results[i].delivery_date__c;
+							output += '", "TotalAmount":"' + results[i].totalamount;
+							output += '", "Status":"' + results[i].status;
+							output += '", "Note":"' + results[i].note__c;
+							
+							output += '", "IsDeleted":' + results[i].isdeleted;
+							output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
+						}
+						if(results.length)
+						{
+							output = output.substr(0, output.length - 1);
+						}
+						output += ']';
+						console.log(output);
+						res.json(JSON.parse(output));
+						
+					}) 
+					.catch(next); 
+        			}) 
 				.catch(next); 	
 			}
 			catch(ex) { res.status(887).send("{ \"status\": \"fail\" }"); }
