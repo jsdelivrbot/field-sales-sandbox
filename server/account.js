d16787809ba8c436c1720b28527fbb4bb5820948
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-26 17:02:01 +0700

    Update account.js

--- server/account.js
+++ server/account.js
@@ -247,7 +247,7 @@ exports.updateAccount2 = function(req, res, next) {
 		})
 		.catch(next);
 	})
-	.catch(next) { res.status(887).send("{ \"status\": \"fail\" }"); }
+	.catch( res.status(887).send("{ \"status\": \"fail\" }"); );
 			
 	var httprequest = https.request(options, callback);
 	httprequest.on('error', (e) => {
