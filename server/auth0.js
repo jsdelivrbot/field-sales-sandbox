6d48e63745865ad5b5030245349216842d4e6d2d
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-26 16:44:04 +0700

    Update auth0.js

--- server/auth0.js
+++ server/auth0.js
@@ -1,5 +1,32 @@
 exports.authen = function () {
   return new Promise((resolve, reject) => {
-  
+    var https = require('https');
+    var options = {
+      host: 'app98692077.auth0.com',
+      path: '/userinfo',
+      port: '443',
+      method: 'GET',
+      headers: { 'authorization': head }
+    };
+
+    callback = function(results) {
+      var str = '';
+      results.on('data', function(chunk) {
+        str += chunk;
+      });
+      results.on('end', function() {
+        try {
+          console.log(str);
+          var obj = JSON.parse(str);
+        }
+        catch(ex) { res.status(887).send("{ \"status\": \"fail\" }"); }
+      });
+    }
+
+    var httprequest = https.request(options, callback);
+    httprequest.on('error', (e) => {
+      res.send('problem with request: ${e.message}');
+    });
+    httprequest.end();	
   })
 }
