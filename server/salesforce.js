e65bd22271fa65dcd2fd0d11130de97b51020f48
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-26 15:20:42 +0700

    Update salesforce.js

--- server/salesforce.js
+++ server/salesforce.js
@@ -1,17 +1,21 @@
 exports.authen = function () {
 	return new Promise((resolve, reject) => {
 		var https = require('https');
+		var postBody = JSON.stringify({      
+			'grant_type': 'password',
+			'client_id': '3MVG99S6MzYiT5k9JoKu1gD1XepU0fFGE_cjs7rc3m2trKegyWnlmuL_c4W4Z4S_JBEoIRxfVN9SzbE8ZH3f1',
+			'client_secret': '8905248785196363462',
+			'username': 'itthiphum.l@thaiunion.co.dev',
+			'password': 'apassword11ovxQJr78JijI9xHLO11JnNjn'
+		});
 		var options = {
 		host: 'test.salesforce.com',
 		path: '/services/oauth2/token',
 		port: '443',
 		method: 'POST',
-		headers: { 'grant_type': 'password',
-			 'client_id': '3MVG99S6MzYiT5k9JoKu1gD1XepU0fFGE_cjs7rc3m2trKegyWnlmuL_c4W4Z4S_JBEoIRxfVN9SzbE8ZH3f1',
-			 'client_secret': '8905248785196363462',
-			 'username': 'itthiphum.l@thaiunion.co.dev',
-			 'password': 'apassword11ovxQJr78JijI9xHLO11JnNjn'
-		       }
+		headers: { 'Content-Type': 'application/json',
+			   'Content-Length': Buffer.byteLength(postBody)
+			 }
 		};
 		callback = function(results) {
 			var str = '';
