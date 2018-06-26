53dc67f02c86a7f092907759b7bc6e31ca4d8f4b
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-26 14:24:30 +0700

    Update callvisit.js

--- server/callvisit.js
+++ server/callvisit.js
@@ -138,3 +138,21 @@ exports.getList = function(req, res, next) {
 	});
 	httprequest.end();	
 };
+
+exports.createCallVisit2 = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+
+	var query = "INSERT INTO salesforce.call_visit__c ( Name, Account__c, Salesman__c, Plan_Start__c, ";
+	query += "Plan_End__c, Call_Type__c, Status__c, Comment__c, createddate, systemmodstamp, ";
+	query += "IsDeleted ) VALUES ('";
+	query += req.body.name + "', '" + req.body.account + "', '" + req.body.salesman + "', '";
+	query += req.body.start + "', '" + req.body.end + "', 'Unplanned', '" + req.body.status;
+	query += req.body.comment + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false)";
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
