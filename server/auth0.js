831c0aaf1824c1332981fef52450942b123dc0df
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-27 10:03:08 +0700

    Update auth0.js

--- server/auth0.js
+++ server/auth0.js
@@ -1,4 +1,7 @@
 var hostname = 'app98692077.auth0.com';
+var client = 'Ko42sNQ96ngSP1KTvs6FScGHPXThIwn6';
+var connection = 'Username-Password-Authentication';
+
 
 exports.authen = function (head) {
   return new Promise((resolve, reject) => {
@@ -29,5 +32,40 @@ exports.authen = function (head) {
     var httprequest = https.request(options, callback);
     httprequest.on('error', (e) => { reject(e); });
     httprequest.end();	
+  })
+}
+
+exports.signup = function (user, email, pass) {
+  return new Promise((resolve, reject) => {
+    var https = require('https');
+    var postBody = JSON.stringify({      
+      'client_id': client,
+      'username': user,
+      'email': email,
+      'password': pass,
+      'connection': connection
+    });
+    var options = {
+      host: hostname,
+      path: '/dbconnections/signup',
+      port: '443',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postBody)
+      }
+    };
+    callback = function(results) {
+		var str = '';
+		results.on('data', function(chunk) {
+			str += chunk;
+		});
+		results.on('end', function() {
+			try {
+				var obj = JSON.parse(str);
+        resolve(obj);
+        }
+        catch(ex) { reject(ex); }
+      });
+    }
   })
 }
