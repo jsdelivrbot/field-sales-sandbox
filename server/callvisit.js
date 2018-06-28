22bdfe49ffe04187bf8104f95bc2ac620b502cea
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-28 16:06:23 +0700

    Update callvisit.js

--- server/callvisit.js
+++ server/callvisit.js
@@ -161,8 +161,8 @@ exports.updateCallVisit2 = function(req, res, next) {
 				var query = "UPDATE salesforce.call_visit__c SET ";
 				query += "Name = '" + req.body.Name + "', ";
 				query += "Account__c = '" + req.body.Account__c + "', ";
-				query += "Plan_Start__c = '" + req.body.Plan_Start__c + "', ";
-				query += "Plan_End__c = '" + req.body.Plan_End__c + "', ";
+				query += "Plan_Start__c = '" + req.body.Plan_Start__c.toISOString() + "', ";
+				query += "Plan_End__c = '" + req.body.Plan_End__c.toISOString() + "', ";
 				query += "Comment__c = '" + req.body.Comment__c + "', ";
 				query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 				query += "WHERE sfid = '" + id + "'";
