f4366a229d434bafc3cffa832848d0c8b1467dcf
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-29 12:02:44 +0700

    Update account.js

--- sync/account.js
+++ sync/account.js
@@ -3,8 +3,18 @@ var db = require('./pghelper');
 exports.sync = function(req, res, next) {
   var head = req.headers['authorization'];
   var lastsync = req.headers['lastsync'];
-  
-  if (!req.body) return res.sendStatus(400);
-  
-  res.send('{ \"status\": "success" }');
+
+  auth.authen(head)
+	.then(function(obj) {
+		var sales = obj.nickname;
+    var query = "SELECT * FROM salesforce.Account WHERE sfid IN ";
+    query += "(SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales + "'";
+    query += "' ) and systemmodstamp > '" + lastsync + "' by accountnumber asc";
+    db.select(query)
+    .then(function(results) {
+
+    }) 
+    .catch(next);
+    res.send('{ \"status\": "success" }');
+  }, function(err) { res.status(887).send("{ \"status\": \"fail\" }"); })
 };
