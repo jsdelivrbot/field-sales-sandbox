68db82800004bd6d5907017323ffe85478856aff
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-29 16:34:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -95,6 +95,9 @@ app.post('/createinvoice', jsonParser, invoice.createInvoice);
 app.post('/updateinvoice/:id', jsonParser, invoice.updateInvoice);
 app.get('/deleteinvoice/:id', invoice.deleteInvoice);
 
+var account2 = require('./sync/account')
+app.get('/syncaccount', account2.sync);
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
