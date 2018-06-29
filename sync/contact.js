4320a144b3ec9a77671c245340b2429acca41fd0
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-06-29 17:24:21 +0700

    Update contact.js

--- sync/contact.js
+++ sync/contact.js
@@ -1,4 +1,5 @@
 var db = require('./pghelper');
+var fs = require('fs');
 
 exports.sync = function(req, res, next) {
 	var head = req.headers['authorization'];
@@ -7,23 +8,35 @@ exports.sync = function(req, res, next) {
 	auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT * FROM salesforce.Contact WHERE sfid IN ";
+		var query = "SELECT * FROM salesforce.Account WHERE sfid IN ";
+		query += "(SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales + "'";
+		query += "' ) by accountnumber asc";
 		db.select(query) 
 		.then(function(results) {
-			var output = '[';
-			for(var i = 0 ; i < results.length ; i++)
+			if(results.length > 0)
 			{
-				output += '{"InternalId":"' + results[i].sfid;
+				var accountList = "(";
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					accountList += "'" + results[i].sfid + "', ";
+				}
+				accountList = accountList.substr(0, accountList.length - 2);
+				accountList += ")";
 				
-				output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
+				var query2 = "SELECT * FROM salesforce.Contact WHERE systemmodstamp > '" + lastsync + "' ";
+				query2 += "and accountId IN " + accountList;
+				console.log(query2);
+				db.select(query2)
+				.then(function(results2) 
+					fs.writeFile("/data/contact" + Date.now() + ".json", req.body, function(err) {
+					    if(err) { return console.log(err); }
+					    console.log("The file was saved!");
+					}); 
+				      //res.json(JSON.parse(output));
+				      res.send("Finish!!");
+				}) 
+				.catch(next);
 			}
-			if(results.length)
-			{
-				output = output.substr(0, output.length - 1);
-			}
-			output += ']';
-			console.log(output);
-			res.json(JSON.parse(output));
 		}) 
 		.catch(next);
 	}, function(err) { res.status(887).send("{ \"status\": \"fail\" }"); })
