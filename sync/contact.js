058b95e1fc3720b9d5dcd6030efd302007a5ec31
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-02 14:55:04 +0700

    Update contact.js

--- sync/contact.js
+++ sync/contact.js
@@ -23,7 +23,7 @@ exports.sync = function(req, res, next) {
 				accountList = accountList.substr(0, accountList.length - 2);
 				accountList += ")";
 				
-				var query2 = "SELECT guid, Firstname, Lastname, Nickname__c, Department, Title as Position,
+				var query2 = "SELECT guid, Firstname, Lastname, Nickname__c, Department, Title as Position, ";
 				query2 += "Phone, Mobile, Email, AccountId as Account FROM salesforce.Contact WHERE accountId IN " + accountList;
 				db.select(query2)
 				.then(function(results2) {
