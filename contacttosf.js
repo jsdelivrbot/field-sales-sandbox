9c20ded723c4aed6795869663bad968ef726aac0
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-03 11:18:44 +0700

    Update contacttosf.js

--- contacttosf.js
+++ contacttosf.js
@@ -10,7 +10,8 @@ db.select(query)
 		.then(function(results2) {	
 			//Build results
 			var lstGUID = [];
-			var body = body2 = '{ "allOrNone" : false, "records" : [';
+			var body = '{ "allOrNone" : false, "records" : [';
+			var body2 = '{ "allOrNone" : false, "records" : [';
 			for(var i = 0 ; i < results.length ; i++)
 			{
 				if(results[i].sfid != null)
@@ -25,7 +26,6 @@ db.select(query)
 					if(results[i].Email != null) body2 += '"Email":"' + results[i].Email + '", ';
 					if(results[i].Department != null) body2 += '"Department":"' + results[i].Department + '", ';
 					if(results[i].Mobile != null) body2 += '"Mobilephone":"' + results[i].Mobile + '"}, ';
-					lstGUID.push(results[i].GUID);
 				}
 				else
 				{
@@ -39,43 +39,52 @@ db.select(query)
 					if(results[i].Email != null) body += '"Email":"' + results[i].Email + '", ';
 					if(results[i].Department != null) body += '"Department":"' + results[i].Department + '", ';
 					if(results[i].Mobile != null) body += '"Mobilephone":"' + results[i].Mobile + '"}, ';
+					lstGUID.push(results[i].GUID);
 				}
 			}
 			body = body.substr(0, body.length - 2);
 			body += ']}';
+			console.debug(body);
 			sf.createComposite(body, results2.token_type + ' ' + results2.access_token)
 			.then(function(results3) {
-				var query2 = 'UPDATE salesforce.Contact SET ';
-				query2 += 'sfid = d.sfid, sync_status = d.sync_status ';
-				query2 += 'from (values ';
-				for(var i = 0 ; i < results3.length ; i++)
+				if(results3.length > 0)
 				{
-					if(results3[i].success == true)	
-						query2 += '("' + lstGUID[i] + '", "' + results3[i].id + '", "Sync"), ';
+					var query2 = 'UPDATE salesforce.Contact SET ';
+					query2 += 'sfid = d.sfid, sync_status = d.sync_status ';
+					query2 += 'from (values ';
+					for(var i = 0 ; i < results3.length ; i++)
+					{
+						if(results3[i].success == true)	
+							query2 += '("' + lstGUID[i] + '", "' + results3[i].id + '", "Sync"), ';
+					}
+					query2 = query2.substr(0, query2.length - 2);
+					query2 += ') as d(guid, sfid, sync_status) where d.guid = guid';
+					db.select(query2)
+					.then(function(results4) {
+
+					}, function(err) { console.debug(err); })
 				}
-				query2 = query2.substr(0, query2.length - 2);
-				query2 += ') as d(guid, sfid, sync_status) where d.guid = guid';
-				db.select(query2)
-				.then(function(results4) {
-					
-				}, function(err) { console.debug(err); })
 			}, function(err) { console.debug(err); })
 			
+			console.debug(body2);
 			sf.updateComposite(body2, results2.token_type + ' ' + results2.access_token)
 			.then(function(results5) {
-				var query3 = 'UPDATE salesforce.Contact SET ';
-				query3 += 'sync_status = d.sync_status ';
-				query3 += 'from (values ';
-				for(var i = 0 ; i < results5.length ; i++)
+				if(results5.length > 0)
 				{
-					if(results5[i].success == true)	query3 += '("' + results5[i].id + '", "Sync"), ';
+					var query3 = 'UPDATE salesforce.Contact SET ';
+					query3 += 'sync_status = d.sync_status ';
+					query3 += 'from (values ';
+					for(var i = 0 ; i < results5.length ; i++)
+					{
+						if(results5[i].success == true)	query3 += '("' + results5[i].id + '", "Sync"), ';
+					}
+					query3 = query3.substr(0, query3.length - 2);
+					query3 += ') as d(sfid, sync_status) where d.sfid = sfid';
+					db.select(query3)
+					.then(function(results6) {
+
+					}, function(err) { console.debug(err); })
 				}
-				query3 = query3.substr(0, query3.length - 2);
-				query3 += ') as d(sfid, sync_status) where d.sfid = sfid';
-				db.select(query3)
-				.then(function(results6) {
-					
-				}, function(err) { console.debug(err); })
 			}, function(err) { console.debug(err); })
 		}, function(err) { console.debug(err); })
 	}
