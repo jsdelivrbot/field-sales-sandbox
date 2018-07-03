9531fdd8b0c7ba8c54c5c9acfb53c1ba20005fdc
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-03 16:44:18 +0700

    Update pricebook.js

--- sync/pricebook.js
+++ sync/pricebook.js
@@ -8,7 +8,7 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT * FROM salesforce.pricebook2 WHERE systemmodstamp > '" + lastsync + "' by Name asc";
+		var query = "SELECT * FROM salesforce.pricebook2 WHERE systemmodstamp > '" + lastsync + "' order by Name asc";
 		db.select(query) 
 		.then(function(results) {
 			var output = '[';
