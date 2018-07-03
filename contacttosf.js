70ea644a274aed7fa76cd46a1ea276c64ef1de5b
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-03 12:18:49 +0700

    Update contacttosf.js

--- contacttosf.js
+++ contacttosf.js
@@ -53,16 +53,16 @@ db.select(query)
 				console.log(results3);
 				if(results3.length > 0)
 				{
-					var query2 = 'UPDATE salesforce.Contact SET ';
+					var query2 = 'UPDATE salesforce.Contact as o SET ';
 					query2 += 'sfid = d.sfid, sync_status = d.sync_status ';
 					query2 += 'from (values ';
 					for(var i = 0 ; i < results3.length ; i++)
 					{
 						if(results3[i].success == true)	
-							query2 += '("' + lstGUID[i] + '", "' + results3[i].id + '", "Sync"), ';
+							query2 += "('" + lstGUID[i] + "', '" + results3[i].id + "', 'Sync'), ";
 					}
 					query2 = query2.substr(0, query2.length - 2);
-					query2 += ') as d(guid, sfid, sync_status) where d.guid = guid';
+					query2 += ') as d(guid, sfid, sync_status) where d.guid = o.guid';
 					db.select(query2)
 					.then(function(results4) {
 
@@ -76,15 +76,15 @@ db.select(query)
 				console.log(results5);
 				if(results5.length > 0)
 				{
-					var query3 = 'UPDATE salesforce.Contact SET ';
+					var query3 = 'UPDATE salesforce.Contact as o SET ';
 					query3 += 'sync_status = d.sync_status ';
 					query3 += 'from (values ';
 					for(var i = 0 ; i < results5.length ; i++)
 					{
-						if(results5[i].success == true)	query3 += '("' + results5[i].id + '", "Sync"), ';
+						if(results5[i].success == true)	query3 += "('" + results5[i].id + "', 'Sync'), ";
 					}
 					query3 = query3.substr(0, query3.length - 2);
-					query3 += ') as d(sfid, sync_status) where d.sfid = sfid';
+					query3 += ') as d(sfid, sync_status) where d.sfid = o.sfid';
 					db.select(query3)
 					.then(function(results6) {
 
