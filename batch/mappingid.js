91ee0f21d3696f14d4851ac98cee164291c5104a
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-04 14:47:49 +0700

    Update mappingid.js

--- batch/mappingid.js
+++ batch/mappingid.js
@@ -1,6 +1,8 @@
 var db = require('../server/pghelper');
 
-var query = "SELECT * FROM salesforce.Order WHERE sync_status = 'Mobile'";
+var query = "SELECT o1.sfid as id, o2.sfid as original, v.sfid as visit ";
+query += "FROM salesforce.Order as o1 inner join salesforce.Order as o2 on o1.originalorder_guid = o2.guid";
+query += "inner join salesforce.call_visit__c as v on v.guid = o1.visit_guid";
 db.select(query)
 .then(function(results) {
   
