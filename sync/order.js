0578d1638edef75041d4ae9dad5b9f5dc7e6afa1
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-04 10:38:23 +0700

    Update order.js

--- sync/order.js
+++ sync/order.js
@@ -23,9 +23,9 @@ exports.sync = function(req, res, next) {
 				accountList = accountList.substr(0, accountList.length - 2);
 				accountList += ")";
 				
-				var query2 = "SELECT guid, Firstname, Lastname, Nickname__c, Department, Title as Position, ";
-				query2 += "Phone, Mobilephone as Mobile, Email, AccountId as Account, systemmodstamp ";
-				query2 += "FROM salesforce.Contact WHERE accountId IN " + accountList;
+				var query2 = "SELECT guid, accountid, ship_to__c, originalorderid, call_visit__c, delivery_date__c, ";
+				query2 += "activateddate, totalamount, status, note__c, is_planned__c, pricebook2id, ordernumber ";
+				query2 += "FROM salesforce.order WHERE accountId IN " + accountList;
 				db.select(query2)
 				.then(function(results2) {
 				      	var output = buildResponse(req.body, results2, lastsync, next)
@@ -73,25 +73,31 @@ function syncDB(update, action, next)
 	{
 		if(action[0] == "insert")
 		{
-			var query = "INSERT INTO salesforce.Contact ( guid, ";
-			if(update[0].Firstname != null) query += "FirstName, ";
-			if(update[0].Lastname != null) query += "LastName, ";
-			if(update[0].Nickanme != null) query += "Nickname__c, ";
-			if(update[0].Phone != null) query += "Phone, ";
-			if(update[0].Position != null) query += "Title, ";
-			if(update[0].Email != null) query += "Email, ";
-			if(update[0].Department != null) query += "Department, ";
-			if(update[0].Account != null) query += "AccountId, ";
+			var query = "INSERT INTO salesforce.order ( guid, ";
+			if(update[0].BillTo != null) query += "accountid, ";
+			if(update[0].ShipTo != null) query += "ship_to__c, ";
+			if(update[0].ParentOrder != null) query += "originalorderid, ";
+			if(update[0].Visit != null) query += "call_visit__c, ";
+			if(update[0].DeliveryDate != null) query += "delivery_date__c, ";
+			if(update[0].OrderDate != null) query += "activateddate, ";
+			if(update[0].TotalAmount != null) query += "totalamount, ";
+			if(update[0].Status != null) query += "status, ";
+			if(update[0].Note != null) query += "note__c, ";
+			if(update[0].IsPlanned != null) query += "is_planned__c, ";
+			if(update[0].Pricebook != null) query += "pricebook2id, ";
 			query += "createddate, systemmodstamp, IsDeleted, sync_status ) VALUES ('";
 			query += update[0].GUID + "',";
-			if(update[0].Firstname != null) query += " '" + update[0].Firstname + "',";
-			if(update[0].Lastname != null) query += " '" + update[0].Lastname + "',";
-			if(update[0].Nickanme != null) query += " '" + update[0].Nickanme + "',";
-			if(update[0].Phone != null) query += " '" + update[0].Phone + "',";
-			if(update[0].Position != null) query += " '" + update[0].Position + "',";
-			if(update[0].Email != null) query += " '" + update[0].Email + "',";
-			if(update[0].Department != null) query += " '" + update[0].Department + "',";
-			if(update[0].Account != null) query += " '" + update[0].Account + "', ";
+			if(update[0].BillTo != null) query += " '" + update[0].BillTo + "',";
+			if(update[0].ShipTo != null) query += " '" + update[0].ShipTo + "',";
+			if(update[0].ParentOrder != null) query += " '" + update[0].ParentOrder + "',";
+			if(update[0].Visit != null) query += " '" + update[0].Visit + "',";
+			if(update[0].DeliveryDate != null) query += " '" + update[0].DeliveryDate + "',";
+			if(update[0].OrderDate != null) query += " '" + update[0].OrderDate + "',";
+			if(update[0].TotalAmount != null) query += " '" + update[0].TotalAmount + "',";
+			if(update[0].Status != null) query += " '" + update[0].Status + "', ";
+			if(update[0].Note != null) query += " '" + update[0].Note + "', ";
+			if(update[0].IsPlanned != null) query += " " + update[0].IsPlanned + ", ";
+			if(update[0].Pricebook != null) query += " '" + update[0].Pricebook + "', ";
 			query += "CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, 'Mobile')";
 
 			db.select(query)
@@ -104,16 +110,18 @@ function syncDB(update, action, next)
 		}
 		else if (action[0] == "update")
 		{
-			var query = "UPDATE salesforce.Contact SET ";
-			if(update[0].Account != null) query += "AccountId = '" + update[0].Account + "', ";
-			if(update[0].Firstname != null) query += "Firstname = '" + update[0].Firstname + "', ";
-			if(update[0].Lastname != null) query += "Lastname = '" + update[0].Lastname + "', ";
-			if(update[0].Nickanme != null) query += "Nickname__c = '" + update[0].Nickanme + "', ";
-			if(update[0].Phone != null) query += "Phone = '" + update[0].Phone + "', ";
-			if(update[0].Position != null) query += "Title = '" + update[0].Position + "', ";
-			if(update[0].Email != null) query += "Email = '" + update[0].Email + "', ";
-			if(update[0].Department != null) query += "Department = '" + update[0].Department + "', ";
-			if(update[0].Mobile != null) query += "Mobilephone = '" + update[0].Mobile + "', ";
+			var query = "UPDATE salesforce.order SET ";
+			if(update[0].BillTo != null) query += "accountid = '" + update[0].BillTo + "', ";
+			if(update[0].ShipTo != null) query += "ship_to__c = '" + update[0].ShipTo + "', ";
+			if(update[0].ParentOrder != null) query += "originalorderid = '" + update[0].ParentOrder + "', ";
+			if(update[0].Visit != null) query += "call_visit__c = '" + update[0].Visit + "', ";
+			if(update[0].DeliveryDate != null) query += "delivery_date__c = '" + update[0].DeliveryDate + "', ";
+			if(update[0].OrderDate != null) query += "activateddate = '" + update[0].OrderDate + "', ";
+			if(update[0].TotalAmount != null) query += "totalamount = '" + update[0].TotalAmount + "', ";
+			if(update[0].Status != null) query += "status = '" + update[0].Status + "', ";
+			if(update[0].Note != null) query += "note__c = '" + update[0].Note + "', ";
+			if(update[0].IsPlanned != null) query += "is_planned__c = " + update[0].IsPlanned + ", ";
+			if(update[0].Pricebook != null) query += "pricebook2id = '" + update[0].Pricebook + "', ";
 			if(update[0].IsDeleted != null) query += "Isdeleted = '" + update[0].IsDeleted +"', ";
 			query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 			query += "sync_status = 'Mobile' ";
