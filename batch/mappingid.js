352aca9ce7b45b4d9a6017e9b43bb33ad29f555d
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-04 15:11:32 +0700

    Update mappingid.js

--- batch/mappingid.js
+++ batch/mappingid.js
@@ -1,9 +1,9 @@
 var db = require('../server/pghelper');
 
 var query = "SELECT o1.sfid as id, o2.sfid as original, v.sfid as visit ";
-query += "FROM salesforce.Order as o1 inner join salesforce.Order as o2 on o1.originalorder_guid = o2.guid";
-query += "inner join salesforce.call_visit__c as v on v.guid = o1.visit_guid";
-query += "WHERE (o1.originalorder_guid IS NOT NULL and o1.originalorderid IS NULL) or";
+query += "FROM salesforce.Order as o1 inner join salesforce.Order as o2 on o1.originalorder_guid = o2.guid ";
+query += "inner join salesforce.call_visit__c as v on v.guid = o1.visit_guid ";
+query += "WHERE (o1.originalorder_guid IS NOT NULL and o1.originalorderid IS NULL) or ";
 query += "(o1.visit_guid IS NOT NULL and o1.call_visit__c IS NULL)";
 db.select(query)
 .then(function(results) {
