9e97d9f96f018d6b01b8cd2f42b291f6f5f26dc9
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-05 16:07:00 +0700

    Update ordertosf.js

--- batch/ordertosf.js
+++ batch/ordertosf.js
@@ -50,9 +50,9 @@ db.select(query)
 				}
 			}
 			body = body.substr(0, body.length - 2);
-			body += '}]}';
+			body += ']}';
 			body2 = body2.substr(0, body2.length - 2);
-			body2 += '}]}';
+			body2 += ']}';
 			if(lstGUID.length > 0)
 			{
 				console.log(body);
