b48897093740d2b39b118ad3cf3d066884405424
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-05 16:03:25 +0700

    Update ordertosf.js

--- batch/ordertosf.js
+++ batch/ordertosf.js
@@ -53,50 +53,56 @@ db.select(query)
 			body += '}]}';
 			body2 = body2.substr(0, body2.length - 2);
 			body2 += '}]}';
-			console.log(body);
-			sf.createComposite(body, results2.token_type + ' ' + results2.access_token)
-			.then(function(results3) {
-				console.log(results3);
-				if(results3.length > 0)
-				{
-					var query2 = 'UPDATE salesforce.order as o SET ';
-					query2 += 'sfid = d.sfid, sync_status = d.sync_status ';
-					query2 += 'from (values ';
-					for(var i = 0 ; i < results3.length ; i++)
+			if(lstGUID.length > 0)
+			{
+				console.log(body);
+				sf.createComposite(body, results2.token_type + ' ' + results2.access_token)
+				.then(function(results3) {
+					console.log(results3);
+					if(results3.length > 0)
 					{
-						if(results3[i].success == true)	
-							query2 += "('" + lstGUID[i] + "', '" + results3[i].id + "', 'Sync'), ";
-					}
-					query2 = query2.substr(0, query2.length - 2);
-					query2 += ') as d(guid, sfid, sync_status) where d.guid = o.guid';
-					db.select(query2)
-					.then(function(results4) {
-						
-					}, function(err) { console.log(err); })
-				}
-			}, function(err) { console.log(err); })
-			
-			console.log(body2);
-			sf.updateComposite(body2, results2.token_type + ' ' + results2.access_token)
-			.then(function(results5) {
-				console.log(results5);
-				if(results5.length > 0)
-				{
-					var query3 = 'UPDATE salesforce.order as o SET ';
-					query3 += 'sync_status = d.sync_status ';
-					query3 += 'from (values ';
-					for(var i = 0 ; i < results5.length ; i++)
-					{
-						if(results5[i].success == true)	query3 += "('" + results5[i].id + "', 'Sync'), ";
-					}
-					query3 = query3.substr(0, query3.length - 2);
-					query3 += ') as d(sfid, sync_status) where d.sfid = o.sfid';
-					db.select(query3)
-					.then(function(results6) {
+						var query2 = 'UPDATE salesforce.order as o SET ';
+						query2 += 'sfid = d.sfid, sync_status = d.sync_status ';
+						query2 += 'from (values ';
+						for(var i = 0 ; i < results3.length ; i++)
+						{
+							if(results3[i].success == true)	
+								query2 += "('" + lstGUID[i] + "', '" + results3[i].id + "', 'Sync'), ";
+						}
+						query2 = query2.substr(0, query2.length - 2);
+						query2 += ') as d(guid, sfid, sync_status) where d.guid = o.guid';
+						db.select(query2)
+						.then(function(results4) {
 
-					}, function(err) { console.log(err); })
-				}
-			}, function(err) { console.log(err); })
+						}, function(err) { console.log(err); })
+					}
+				}, function(err) { console.log(err); })
+			}
+			
+			if(results.length > lstGUID.length)
+			{
+				console.log(body2);
+				sf.updateComposite(body2, results2.token_type + ' ' + results2.access_token)
+				.then(function(results5) {
+					console.log(results5);
+					if(results5.length > 0)
+					{
+						var query3 = 'UPDATE salesforce.order as o SET ';
+						query3 += 'sync_status = d.sync_status ';
+						query3 += 'from (values ';
+						for(var i = 0 ; i < results5.length ; i++)
+						{
+							if(results5[i].success == true)	query3 += "('" + results5[i].id + "', 'Sync'), ";
+						}
+						query3 = query3.substr(0, query3.length - 2);
+						query3 += ') as d(sfid, sync_status) where d.sfid = o.sfid';
+						db.select(query3)
+						.then(function(results6) {
+
+						}, function(err) { console.log(err); })
+					}
+				}, function(err) { console.log(err); })
+			}
 		}, function(err) { console.log(err); })
 	}
 }, function(err) { console.log(err); })
