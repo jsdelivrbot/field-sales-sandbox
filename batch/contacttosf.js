21ae842f417ff5f18785092a568c24cc3930ff4a
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-06 14:44:53 +0700

    Update contacttosf.js

--- batch/contacttosf.js
+++ batch/contacttosf.js
@@ -30,7 +30,7 @@ db.select(query)
 					if(results[i].department != null) body2 += '"Department":"' + results[i].department + '", ';
 					if(results[i].mobilephone != null) body2 += '"Mobilephone":"' + results[i].mobilephone + '", ';
 					//if(results[i].isdeleted != null) body2 += '"IsDeleted":' + results[i].isdeleted + ', ';
-					body2 += '"Source__c":"App"}';
+					body2 += '"Source__c":"App"}, ';
 					countupdate++;
 				}
 				else
@@ -46,7 +46,7 @@ db.select(query)
 					if(results[i].department != null) body += '"Department":"' + results[i].department + '", ';
 					if(results[i].mobilephone != null) body += '"Mobilephone":"' + results[i].mobilephone + '", ';
 					//if(results[i].isdeleted != null) body += '"IsDeleted":' + results[i].isdeleted + ', ';
-					body += '"Source__c":"App"}';
+					body += '"Source__c":"App"}, ';
 					lstGUID.push(results[i].guid);
 					countinsert++;
 				}
