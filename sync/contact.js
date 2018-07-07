2d9880ba68e8960fc89455ffa22de191c534f952
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-07 07:59:15 +0700

    Update contact.js

--- sync/contact.js
+++ sync/contact.js
@@ -35,7 +35,7 @@ exports.sync = function(req, res, next) {
 				
 				var query2 = "SELECT guid, Firstname, Lastname, Nickname__c, Department, Title as Position, ";
 				query2 += "Phone, Mobilephone as Mobile, Email, AccountId as Account, IsDeleted, success as Success, ";
-				query2 += "errorcode as ErrorCode, errormessage as ErrorMessage, systemmodstamp as UpdatedDate";
+				query2 += "errorcode as ErrorCode, errormessage as ErrorMessage, systemmodstamp as UpdatedDate ";
 				query2 += "FROM salesforce.Contact WHERE (accountId IN " + accountList + " and ";
 				query2 += "systemmodstamp > '" + lastsync2 + "') or guid IN " + contactlist;
 				db.select(query2)
