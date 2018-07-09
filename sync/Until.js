ce86e8d54d8ebc670009ee70c080cbb8d49a5f26
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-09 15:12:31 +0700

    Create Until.js

---
+++ sync/Until.js
@@ -0,0 +1,12 @@
+exports.authen = function (txtDate) {
+  if (txtDate!='')
+    return '';
+  
+  var data_array = data.split(' '); 
+  var returndata='';
+  for(var i = 0; i < data_array.length; i++){
+  
+  }
+  
+  return txtDate; 
+}
