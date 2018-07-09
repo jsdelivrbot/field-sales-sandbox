c1f1c1e5ad80a46c7462c7321e7e048a0229e980
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-09 15:45:21 +0700

    Update Util.js

--- sync/Util.js
+++ sync/Util.js
@@ -4,10 +4,21 @@ exports.convert = function (txtDate) {
     return '';
   
   var data_array = txtDate.split(' '); 
-  var returndata='';
-  for(var i = 0; i < data_array.length; i++){
+  var returndata=''+;
+  try{
+    //if (data_array[2]!=''){
+    
+    //}
+    
+    //returndata=data_array[3]+'-'+data_array[1]+'-'+data_array[2]+' '+data_array[4]
+  }catch (Exception ex){
+    //can not split data
+    returndata=txtDate;
+  } 
+  //for(var i = 0; i < data_array.length; i++){
   
-  }
+  //}
+  
   
   return txtDate; 
 }
