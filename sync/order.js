da1c9d721d57bde2d82cf5eafc7b1789ce399b0d
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-09 16:25:04 +0700

    Update order.js

--- sync/order.js
+++ sync/order.js
@@ -23,7 +23,7 @@ exports.sync = function(req, res, next) {
 			orderlist += ")";
 			
 			var query2 = "SELECT guid, accountid, ship_to__c, originalorder_guid, visit_guid,  to_char( delivery_date__c, 'YYYY-MM-DD') as delivery_date__c, ";
-			query2 += "activateddate, totalamount, status, note__c, is_planned__c, ordernumber, success as Success, ";
+			query2 += " to_char( activateddate, 'YYYY-MM-DD') as activateddate , totalamount, status, note__c, is_planned__c, ordernumber, success as Success, ";
 			query2 += "errorcode as ErrorCode, errormessage as ErrorMessage, to_char( systemmodstamp, 'YYYY-MM-DD HH:MI:SS') as updatedate , isdeleted ";
 			query2 += "FROM salesforce.order WHERE (LOWER(salesman__c) = '" + sales + "' and ";
 			query2 += "systemmodstamp > '" + lastsync2 + "') or guid IN " + orderlist;
