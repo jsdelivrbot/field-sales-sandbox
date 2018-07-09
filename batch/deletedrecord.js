4a232d4ce65c608aa766fffe6ed08d1978071c16
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-09 10:15:39 +0700

    Update deletedrecord.js

--- batch/deletedrecord.js
+++ batch/deletedrecord.js
@@ -1,18 +1,102 @@
 var db = require('../server/pghelper');
 
-var query = "DELETE FROM salesforce.contact WHERE isdeleted = true ";
+var query = "DELETE FROM salesforce.order_product__c WHERE isdeleted = true RETURNING *";
 db.select(query)
 .then(function(results) {
 
 }, function(err) { console.log(err); })
 
-var query = "DELETE FROM salesforce.account_team__c WHERE isdeleted = true ";
+var query = "DELETE FROM salesforce.order__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+  
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.scale_price__c WHERE isdeleted = true RETURNING *";
 db.select(query)
 .then(function(results) {
 
 }, function(err) { console.log(err); })
 
-var query = "DELETE FROM salesforce.account WHERE isdeleted = true ";
+var query = "DELETE FROM salesforce.pricebook_entry__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.pricebook2 WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.product2 WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.promotion__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.salesman__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.top_store_program__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.product_history__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.good_return__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.call_card__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.call_visit__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.invoice__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.contact WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.account_team__c WHERE isdeleted = true RETURNING *";
+db.select(query)
+.then(function(results) {
+
+}, function(err) { console.log(err); })
+
+var query = "DELETE FROM salesforce.account WHERE isdeleted = true RETURNING *";
 db.select(query)
 .then(function(results) {
 
