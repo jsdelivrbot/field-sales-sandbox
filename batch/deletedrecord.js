a644284ec073c780d44281171cb1023647a7d412
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-09 10:46:13 +0700

    Update deletedrecord.js

--- batch/deletedrecord.js
+++ batch/deletedrecord.js
@@ -9,25 +9,7 @@ db.select(query)
 var query = "DELETE FROM salesforce.order__c WHERE isdeleted = true RETURNING *";
 db.select(query)
 .then(function(results) {
-  var orderList = "(";
-	for(var i = 0 ; i < results.length ; i++)
-	{
-		orderList += "'" + results[i].sfid + "', ";
-	}
-	orderList = orderList.substr(0, orderList.length - 2);
-	orderList += ")";
-	
-	var query2 = "DELETE FROM salesforce.order_product__c WHERE order__c IN " + orderList;
-	db.select(query2)
-	.then(function(results2) {
-	
-	}, function(err) { console.log(err); })
-	
-	var query3 = "UPDATE salesforce.order as o SET originalorderid = null WHERE originalorderid IN " + orderList;
-	db.select(query3)
-	.then(function(results3) {
-	
-	}, function(err) { console.log(err); })
+
 }, function(err) { console.log(err); })
 
 var query = "DELETE FROM salesforce.scale_price__c WHERE isdeleted = true RETURNING *";
