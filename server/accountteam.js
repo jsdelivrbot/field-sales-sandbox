70665fb004451ab5456b7b986f9853cad7b25f24
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 14:29:44 +0700

    Update accountteam.js

--- server/accountteam.js
+++ server/accountteam.js
@@ -15,10 +15,53 @@ exports.createAccountTeam = function(req, res, next) {
 	.catch(next);
 };
 
+exports.createAccountTeamList = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+
+	var query = "INSERT INTO salesforce.account_team__c ( sfid, account__c, salesman__c, createddate, systemmodstamp, ";
+	query += "IsDeleted ) VALUES ";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].account + "', '" + req.body[i].sales;
+		query += "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false), ";
+	}
+	if(req.body.length > 0 )
+	{
+		query = query.substr(0, query.length - 2);
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
 exports.deleteAccountTeam = function(req, res, next) {
  	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.account_team__c WHERE sfid = '" + id + "'";	
 	var query = "UPDATE salesforce.account_team__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid = '" + id + "'";
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
+
+exports.deleteAccountTeamList = function(req, res, next) {
+ 	var teamList = "(";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		teamList += "'" + req.body[i].sfid + "', ";
+	}
+	teamList = teamList.substr(0, teamList.length - 2);
+	teamList += ")";
+	//var query = "DELETE FROM salesforce.account_team__c WHERE sfid IN " + teamList;	
+	var query = "UPDATE salesforce.account_team__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid IN " + teamList;
 	console.log(query);
 
 	db.select(query)
