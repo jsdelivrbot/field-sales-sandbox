afecd2176d103654dcfda577a2129327ef6657cc
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 14:49:42 +0700

    Update order.js

--- sync/order.js
+++ sync/order.js
@@ -22,9 +22,9 @@ exports.sync = function(req, res, next) {
 			orderlist = orderlist.substr(0, orderlist.length - 2);
 			orderlist += ")";
 			
-			var query2 = "SELECT guid, accountid, ship_to__c, originalorder_guid, visit_guid,  to_char( delivery_date__c, 'YYYY-MM-DD') as delivery_date__c, ";
-			query2 += " to_char( activateddate, 'YYYY-MM-DD') as activateddate , totalamount, status, note__c, is_planned__c, ordernumber, success as Success, ";
-			query2 += "errorcode as ErrorCode, errormessage as ErrorMessage, to_char( systemmodstamp, 'YYYY-MM-DD HH:MI:SS') as updatedate , isdeleted ";
+			var query2 = "SELECT guid, accountid, ship_to__c, originalorder_guid, visit_guid,  to_char( delivery_date__c + interval '7 hour', 'YYYY-MM-DD') as delivery_date__c, ";
+			query2 += " to_char( activateddate + interval '7 hour', 'YYYY-MM-DD') as activateddate , totalamount, status, note__c, is_planned__c, ordernumber, success as Success, ";
+			query2 += "errorcode as ErrorCode, errormessage as ErrorMessage, to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updatedate , isdeleted ";
 			query2 += "FROM salesforce.order WHERE (LOWER(salesman__c) = '" + sales + "' and ";
 			query2 += "systemmodstamp > '" + lastsync2 + "') or guid IN " + orderlist;
 			db.select(query2)
