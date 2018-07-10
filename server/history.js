085a345c69ed46d849e15915ccfb662e6d3c5597
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 15:10:21 +0700

    Update history.js

--- server/history.js
+++ server/history.js
@@ -44,9 +44,9 @@ exports.updateHistory = function(req, res, next) {
 	var id = req.params.id;
 	if (!req.body) return res.sendStatus(400);
   
-	var query = "UPDATE salesforce.product_History__c SET ";
+	var query = "UPDATE salesforce.product_history__c SET ";
 	query += "Name = '" + req.body.name + "', ";
-	query += "Account__c = '" + req.body.accpunt + "', ";
+	query += "Account__c = '" + req.body.account + "', ";
 	query += "Product__c = '" + req.body.product + "', ";
 	query += "systemmodstamp = CURRENT_TIMESTAMP, ";
 	query += "Isdeleted = '" + req.body.isdeleted +"' ";
@@ -60,10 +60,59 @@ exports.updateHistory = function(req, res, next) {
 	.catch(next);
 };
 
+exports.updateHistoryList = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+  
+	var query = "UPDATE salesforce.product_history__c SET ";
+	query += "Name = d.Name, ";
+	query += "Account__c = d.Account__c, ";
+	query += "Product__c = d.Product__c, ";
+	query += 'systemmodstamp = CURRENT_TIMESTAMP from (values ';
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].name + "', '" + req.body[i].account + "', '";
+		query += req.body[i].product + "' "
+		query += "), ";
+	}
+	if(req.body.length > 0)
+	{
+		query = query.substr(0, query.length - 2);
+		query += ") as d(sfid, Name, Account__c, Product__c ";
+		query += ") WHERE o.sfid = d.sfid";
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
 exports.deleteHistory = function(req, res, next) {
 	var id = req.params.id;
   	//var query = "DELETE FROM salesforce.product_History__c WHERE sfid = '" + id + "'";	
 	var query = "UPDATE salesforce.product_History__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
+
+exports.deleteHistoryList = function(req, res, next) {
+	var hostoryList = "(";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		hostoryList += "'" + req.body[i].sfid + "', ";
+	}
+	hostoryList = hostoryList.substr(0, hostoryList.length - 2);
+	hostoryList += ")";
+  	//var query = "DELETE FROM salesforce.product_history__c WHERE sfid IN " + hostoryList;	
+	var query = "UPDATE salesforce.product_history__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid IN " + hostoryList; 
 	console.log(query);
 
 	db.select(query)
