404914c199c55941462fced50c6fb3bf08fc7ee4
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 14:55:28 +0700

    Update account.js

--- server/account.js
+++ server/account.js
@@ -329,8 +329,8 @@ exports.updateAccountList = function(req, res, next) {
 		query += req.body[i].country + "', " + req.body[i].creditlimit + ", '";
 		query += req.body[i].fax + "', '" + req.body[i].faxext + "', '" + req.body[i].phone + "', '" + req.body[i].pricebook + "', '";
 		query += req.body[i].salesdistrict + "', '" + req.body[i].taxnumber + "', '" + req.body[i].industry + "', '" + req.body[i].subindustry + "', '";
-		query += req.body[i].maincontact + "', '" + req.body[i].paymentterm + "', '" + req.body[i].region + "', '" + req.body[i].salesdistrictname + "', '";
-		query += "'), ";
+		query += req.body[i].maincontact + "', '" + req.body[i].paymentterm + "', '" + req.body[i].region + "', '" + req.body[i].salesdistrictname + "' ";
+		query += "), ";
 	}
 	if(req.body.length > 0)
 	{
