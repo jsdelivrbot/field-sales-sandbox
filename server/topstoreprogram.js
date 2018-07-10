62f45871a577730723cde918050cf61c045d9225
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 17:00:20 +0700

    Update topstoreprogram.js

--- server/topstoreprogram.js
+++ server/topstoreprogram.js
@@ -16,6 +16,31 @@ exports.createTopStore = function(req, res, next) {
 	.catch(next);
 };
 
+exports.createTopStoreList = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+
+	var query = "INSERT INTO salesforce.Top_Store_Program__c ( sfid, guid, Name, Account__c, Date__c, Event_Type__c, ";
+	query += "createddate, systemmodstamp, IsDeleted ) VALUES ";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].sfid + "', '" + req.body[i].name + "', ";
+		query += (req.body[i].account != null ? "'" + req.body[i].account + "'" : "null") + ", '" + req.body[i].date + "', '";
+		query += req.body[i].type + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false), ";
+	}
+	if(req.body.length > 0 )
+	{
+		query = query.substr(0, query.length - 2);
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
 exports.updateTopStore = function(req, res, next) {
 	var id = req.params.id;
 	if (!req.body) return res.sendStatus(400);
@@ -37,10 +62,59 @@ exports.updateTopStore = function(req, res, next) {
 	.catch(next);
 };
 
+exports.updateTopStoreList = function(req, res, next) {
+	var id = req.params.id;
+	if (!req.body) return res.sendStatus(400);
+
+	var query = "UPDATE salesforce.Top_Store_Program__c SET ";
+	query += "Name = d.Name, Account__c = d.Account__c, Date__c = d.Date__c, Event_Type__c = d.Event_Type__c, ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP from (values ";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].name + "', ";
+		query += (req.body[i].account != null ? "'" + req.body[i].account + "'" : "null") + "', '" + req.body[i].date + "', '";
+		query += req.body[i].type + "' ";
+		query += "), ";
+	}
+	if(req.body.length > 0)
+	{
+		query = query.substr(0, query.length - 2);
+		query += ") as d(sfid, Name, Account__c, Date__c, Event_Type__c ";
+		query += ") WHERE o.sfid = d.sfid";
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
 exports.deleteTopStore = function(req, res, next) {
 	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.Top_Store_Program__c WHERE sfid = '" + id + "'";	
 	var query = "UPDATE salesforce.Top_Store_Program__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
+
+exports.deleteTopStoreList = function(req, res, next) {
+	var programList = "(";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		programList += "'" + req.body[i].sfid + "', ";
+	}
+	programList = programList.substr(0, programList.length - 2);
+	programList += ")";
+	//var query = "DELETE FROM salesforce.Top_Store_Program__c WHERE sfid IN " + programList;	
+	var query = "UPDATE salesforce.Top_Store_Program__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid IN " + programList; 
 	console.log(query);
 
 	db.select(query)
