cfcb03bb8c49fc93e123e24450680dd8d59ad520
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 15:13:20 +0700

    Update pricebookentry.js

--- sync/pricebookentry.js
+++ sync/pricebookentry.js
@@ -8,7 +8,7 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp, 'YYYY-MM-DD HH:MI:SS') as updatedate FROM salesforce.pricebook_entry__c WHERE systemmodstamp > '" + lastsync + "' ";
+		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.pricebook_entry__c WHERE systemmodstamp > '" + lastsync + "' ";
 		db.select(query) 
 		.then(function(results) {
 			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
