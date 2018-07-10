a20f1ab26ec859fb0fd8073c15280ac096616990
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-10 12:42:06 +0700

    Update pricebook.js

--- sync/pricebook.js
+++ sync/pricebook.js
@@ -25,7 +25,7 @@ exports.sync = function(req, res, next) {
 			{
 				output = output.substr(0, output.length - 1);
 			}
-			output += ']';
+			output += ']}';
 			console.log(output);
 			res.json(JSON.parse(output));
 		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
