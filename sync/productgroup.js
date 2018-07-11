78f47226a8660c4734cb88aac4f86973724262ed
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-11 14:47:06 +0700

    Update productgroup.js

--- sync/productgroup.js
+++ sync/productgroup.js
@@ -31,7 +31,7 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.product_group__c WHERE systemmodstamp > '" + lastsync + "' order by sfid asc";
+		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.product_group__c";
 		db.select(query) 
 		.then(function(results) {
 			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
