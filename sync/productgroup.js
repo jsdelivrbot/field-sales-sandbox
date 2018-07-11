0c12b457ed8d551726d9e14abf5cee0c9e304fd2
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-11 10:40:39 +0700

    Create productgroup.js

---
+++ sync/productgroup.js
@@ -0,0 +1,19 @@
+var db = require('./pghelper');
+var auth = require('../server/auth0');
+
+exports.getProductGroup = function(req, res, next) {
+	var query = "select * from (";
+	query += "select null as parent ,Product_group__c as group, 'Brand' as columnname from salesforce.Product2 Group by Product_group__c ";
+	query += "Union All ";
+	query += "select Product_group__c as Parent , Family as group, 'Family' as columnname FROM salesforce.product2 group by (Product_group__c,Family) ";
+	query += "Union All ";
+	query += "select Family as Parent , Product_type__c as group, 'Type' as columnname FROM salesforce.product2 group by (Family,Product_type__c)";
+	query += ") as t order by columnname asc";
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+}
