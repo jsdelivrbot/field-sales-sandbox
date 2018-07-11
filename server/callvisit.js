41b667a4912cda5f529cb9a881315a92eb9085a8
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-11 16:15:13 +0700

    Update callvisit.js

--- server/callvisit.js
+++ server/callvisit.js
@@ -20,6 +20,33 @@ exports.createCallVisit = function(req, res, next) {
 	.catch(next);
 };
 
+exports.createCallVisitList = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+
+	var query = "INSERT INTO salesforce.call_visit__c ( sfid, guid, Name, Account__c, Salesman__c, Plan_Start__c, ";
+	query += "Plan_End__c, Call_Type__c, Status__c, Comment__c, createddate, systemmodstamp, ";
+	query += "IsDeleted ) VALUES ";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].sfid + "', '" + req.body[i].name + "', '";
+		query += req.body[i].account + "', '" + req.body[i].salesman + "', '" + req.body[i].start + "', '";
+		query += req.body[i].end + "', '" + req.body[i].calltype + "', '" + req.body[i].status + "', '";
+		query += req.body[i].comment + "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false), ";
+	}
+	if(req.body.length > 0 )
+	{
+		query = query.substr(0, query.length - 2);
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
 exports.updateCallVisit = function(req, res, next) {
 	var id = req.params.id;
 	if (!req.body) return res.sendStatus(400);
@@ -51,10 +78,62 @@ exports.updateCallVisit = function(req, res, next) {
 	.catch(next);
 };
 
+exports.updateCallVisitList = function(req, res, next) {
+	var id = req.params.id;
+	if (!req.body) return res.sendStatus(400);
+
+	var query = "UPDATE salesforce.call_visit__c SET ";
+	query += "Name = '" + req.body.name + "', ";
+	query += "Account__c = d.Account__c, Salesman__c = d.Salesman__c, Plan_Start__c = d.Plan_Start__c, Plan_End__c = d.Plan_End__c, ";
+	query += "Call_Type__c = d.Call_Type__c, Status__c = d.Status__c, Comment__c = d.Comment__c, ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP from (values ";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].name + "', '";
+		query += req.body[i].account + "', '" + req.body[i].salesman + "', '" + req.body[i].start + "', '";
+		query += req.body[i].end + "', '" + req.body[i].calltype + "', '" + req.body[i].status + "', '";
+		query += req.body[i].comment + "' ";
+		query += "), ";
+	}
+	if(req.body.length > 0)
+	{
+		query = query.substr(0, query.length - 2);
+		query += ") as d(sfid, Name, Account__c, Salesman__c, Plan_Start__c, Plan_End__c, Call_Type__c, ";
+		query += "Status__c, Comment__c ) WHERE o.sfid = d.sfid";
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
 exports.deleteCallVisit = function(req, res, next) {
 	var id = req.params.id;
 	//var query = "DELETE FROM salesforce.call_visit__c WHERE sfid = '" + id + "'";	
 	var query = "UPDATE salesforce.call_visit__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid ='" + id + "'"; 
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
+
+exports.deleteCallVisitList = function(req, res, next) {
+	var visitList = "(";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		visitList += "'" + req.body[i].sfid + "', ";
+	}
+	visitList = visitList.substr(0, visitList.length - 2);
+	visitList += ")";
+	//var query = "DELETE FROM salesforce.call_visit__c WHERE sfid IN " + visitList;	
+	var query = "UPDATE salesforce.call_visit__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid IN " + visitList; 
 	console.log(query);
 
 	db.select(query)
