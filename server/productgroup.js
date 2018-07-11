a10ad1598420012232975ba5175280e64a3334f6
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-11 14:31:34 +0700

    Update productgroup.js

--- server/productgroup.js
+++ server/productgroup.js
@@ -23,3 +23,51 @@ exports.createGroupList = function(req, res, next) {
 	}
 	else { res.send('{ \"status\": "no data" }'); }
 };
+
+exports.updateGroupList = function(req, res, next) {
+	if (!req.body) return res.sendStatus(400);
+  
+	var query = "UPDATE salesforce.product_group__c as o SET ";
+	query += "name = '" + req.body.name + "', ";
+	query += "column_name__c = '" + req.body.column + "', ";
+	query += "parent__c = " + req.body.parent + ", ";
+	query += "systemmodstamp = CURRENT_TIMESTAMP from (values ";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		query += "('" + req.body[i].sfid + "', '" + req.body[i].name + "', '" + req.body[i].column + "', ";
+		query += req.body[i].parent + " ";
+		query += "), ";
+	}
+	if(req.body.length > 0)
+	{
+		query = query.substr(0, query.length - 2);
+		query += ") as d(sfid, name, column_name__c, parent__c ) WHERE o.sfid = d.sfid";
+		console.log(query);
+
+		db.select(query)
+		.then(function(results) {
+			res.send('{ \"status\": "success" }');
+		})
+		.catch(next);
+	}
+	else { res.send('{ \"status\": "no data" }'); }
+};
+
+exports.deleteGroupList = function(req, res, next) {
+	var groupList = "(";
+	for(var i = 0 ; i < req.body.length ; i++)
+	{
+		groupList += "'" + req.body[i].sfid + "', ";
+	}
+	groupList = groupList.substr(0, groupList.length - 2);
+	groupList += ")";
+  	//var query = "DELETE FROM salesforce.product_group__c WHERE sfid IN " + scaleLgroupListist;	
+	var query = "UPDATE salesforce.product_group__c SET IsDeleted = true, systemmodstamp = CURRENT_TIMESTAMP WHERE sfid IN " + groupList; 
+	console.log(query);
+
+	db.select(query)
+	.then(function(results) {
+		res.send('{ \"status\": "success" }');
+	})
+	.catch(next);
+};
