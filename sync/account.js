ae59d95862a2b8b3c5ecb4f7ad2b61159fc2dde6
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-13 17:31:16 +0700

    Update account.js

--- sync/account.js
+++ sync/account.js
@@ -9,47 +9,53 @@ exports.sync = function(req, res, next) {
 	auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.Account WHERE sfid IN ";
-		query += "(SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales;
-		query += "' ) and systemmodstamp > '" + lastsync + "' order by accountnumber asc";
+		var query = "SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales;
 		db.select(query) 
 		.then(function(results) {
-			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
-			for(var i = 0 ; i < results.length ; i++)
-			{
-				output += '{"id":"' + results[i].guid;
-				output += '", "account_name":"' + results[i].name + ' ' + results[i].account_name_2__c;
-				output += ' ' + results[i].account_name_3__c + ' ' + results[i].account_name_4__c;
-				output += '", "account_number":"' + results[i].accountnumber;
-				output += '", "parent":"' + results[i].parentid;
-				output += '", "tax":"' + results[i].tax_number__c;
-				output += '", "credit_limit":"' + results[i].credit_limit__c;
-				output += '", "phone":"' + results[i].phone;
-				output += '", "fax":"' + results[i].fax +'#' + results[i].fax_ext__c;
-				//output += '", "address no":"' + results[i].address_no__c;
-				//output += '", "address":"' + results[i].address__c;
-				output += '", "address":"' + results[i].address_no__c + " " + results[i].address__c;
-				output += '", "kwang":"' + results[i].kwang__c;
-				output += '", "khet":"' + results[i].khet__c;
-				output += '", "province":"' + results[i].province__c;
-				output += '", "zip":"' + results[i].zip__c;
-				output += '", "country":"' + results[i].country__c;
-				output += '", "pricebook":"' + results[i].price_book__c;
-				output += '", "industry":"' + results[i].industry_name__c;
-				output += '", "subindustry":"' + results[i].industry_code_name__c;
-				output += '", "paymentterm":"' + results[i].payment_term_name__c;
-				output += '", "region":"' + results[i].region_name__c;
-				output += '", "salesdistrict":"' + results[i].sales_district_name__c;
-				output += '", "isdeleted":' + results[i].isdeleted;
-				output += ', "updateddate":"' + results[i].updatedate + '"},';
-			}
-			if(results.length)
-			{
-				output = output.substr(0, output.length - 1);
-			}
-			output += ']}';
-			console.log(output);
-			res.json(JSON.parse(output));
+			var query2 = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.Account WHERE sfid IN ";
+			query2 += "(SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales;
+			query2 += "' ) and systemmodstamp > '" + lastsync + "' order by accountnumber asc";
+			db.select(query2) 
+			.then(function(results2) {
+				var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
+				for(var i = 0 ; i < results2.length ; i++)
+				{
+					output += '{"id":"' + results2[i].guid;
+					output += '", "account_name":"' + results2[i].name + ' ' + results2[i].account_name_2__c;
+					output += ' ' + results2[i].account_name_3__c + ' ' + results2[i].account_name_4__c;
+					output += '", "account_number":"' + results2[i].accountnumber;
+					output += '", "parent":"' + results2[i].parentid;
+					output += '", "tax":"' + results2[i].tax_number__c;
+					output += '", "credit_limit":"' + results2[i].credit_limit__c;
+					output += '", "phone":"' + results2[i].phone;
+					output += '", "fax":"' + results2[i].fax +'#' + results2[i].fax_ext__c;
+					//output += '", "address no":"' + results2[i].address_no__c;
+					//output += '", "address":"' + results2[i].address__c;
+					output += '", "address":"' + results2[i].address_no__c + " " + results2[i].address__c;
+					output += '", "kwang":"' + results2[i].kwang__c;
+					output += '", "khet":"' + results2[i].khet__c;
+					output += '", "province":"' + results2[i].province__c;
+					output += '", "zip":"' + results2[i].zip__c;
+					output += '", "country":"' + results2[i].country__c;
+					output += '", "pricebook":"' + results2[i].price_book__c;
+					output += '", "industry":"' + results2[i].industry_name__c;
+					output += '", "subindustry":"' + results2[i].industry_code_name__c;
+					output += '", "paymentterm":"' + results2[i].payment_term_name__c;
+					output += '", "region":"' + results2[i].region_name__c;
+					output += '", "salesdistrict":"' + results2[i].sales_district_name__c;
+					var division = 'Pet';
+					output += '", "division":"' + division;
+					output += '", "isdeleted":' + results2[i].isdeleted;
+					output += ', "updateddate":"' + results2[i].updatedate + '"},';
+				}
+				if(results.length)
+				{
+					output = output.substr(0, output.length - 1);
+				}
+				output += ']}';
+				console.log(output);
+				res.json(JSON.parse(output));
+			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); 
 		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
 	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
 };
