4fbc23a8d7d41f06a6f705ff41c873370277890a
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-16 15:49:21 +0700

    Update promotion.js

--- sync/promotion.js
+++ sync/promotion.js
@@ -8,25 +8,19 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.Product2 WHERE systemmodstamp > '" + lastsync + "' order by ProductCode asc";
+		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.promotion__c WHERE systemmodstamp > '" + lastsync + "' ";
 		db.select(query) 
 		.then(function(results) {
 			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
 			for(var i = 0 ; i < results.length ; i++)
 			{
 				output += '{"id":"' + results[i].guid;
-				output += '", "product_code":"' + results[i].productcode;
-				output += '", "product_name":"' + results[i].name;
-				output += '", "product_name_th":"' + results[i].Product_Name_TH__c;
-				output += '", "description":"' + results[i].description;
-				output += '", "brand":"' + results[i].product_group__c;
-				output += '", "family":"' + results[i].family;
-				output += '", "type":"' + results[i].product_type__c;
-				output += '", "division":"' + results[i].division__c;
-				output += '", "net_weight":"' + results[i].net_weight_g__c;
-				output += '", "pack_size":"' + results[i].pack_size__c;
-				output += '", "isactive":' + results[i].isactive;
-				output += ', "isdeleted":' + results[i].isdeleted;
+				output += '", "name":"' + results[i].name;
+				output += '", "start":"' + results[i].start_date__c;
+				output += '", "end":"' + results[i].end_date__c;
+				output += '", "description":"' + results[i].description__c;
+				output += '", "url":"' + results[i].url__c;
+				output += '", "isdeleted":' + results[i].isdeleted;
 				output += ', "updateddate":"' + results[i].updatedate + '"},';
 			}
 			if(results.length)
