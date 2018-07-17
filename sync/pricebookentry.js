7697e03844d9b5e16cbe1d5f4c85aa16faab6089
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-17 16:05:44 +0700

    Update pricebookentry.js

--- sync/pricebookentry.js
+++ sync/pricebookentry.js
@@ -8,7 +8,8 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate FROM salesforce.pricebook_entry__c WHERE systemmodstamp > '" + lastsync + "' ";
+		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
+	        query += "FROM salesforce.pricebook_entry__c WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' ";
 		db.select(query) 
 		.then(function(results) {
 			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
@@ -19,7 +20,7 @@ exports.sync = function(req, res, next) {
 				output += '", "pricebook":"' + results[i].price_book__c;
 				output += '", "group":"' + results[i].group__c;
 				output += '", "isdeleted":' + results[i].isdeleted;
-				output += ', "updateddate":"' + results[i].updatedate + '"},';
+				output += ', "updateddate":"' + results[i].updatedate.replace(" ", "T") + '"},';
 			}
 			if(results.length)
 			{
