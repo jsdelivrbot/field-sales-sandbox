20ecf8d5317aa2bd1e4277ae1000e4428c64e436
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-19 08:33:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var util = require('./sync/Util');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+//app.use(express.urlencoded({limit: '50mb'}));
 
 app.get('/test', function(request, response) {
   var date = new Date("2018-07-02 08:30:00");
