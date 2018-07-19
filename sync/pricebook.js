72545cb0d35f9368c1a55c12629e4137d05c7944
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-19 09:41:42 +0700

    Update pricebook.js

--- sync/pricebook.js
+++ sync/pricebook.js
@@ -12,6 +12,7 @@ exports.sync = function(req, res, next) {
 	        query += "FROM salesforce.pricebook2 WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' order by Name asc";
 		db.select(query) 
 		.then(function(results) {
+			/*
 			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
 			for(var i = 0 ; i < results.length ; i++)
 			{
@@ -29,6 +30,15 @@ exports.sync = function(req, res, next) {
 			output += ']}';
 			console.log(output);
 			res.json(JSON.parse(output));
+			*/
+			var output = { "success": true, "errorcode" : "", "errormessage" : "", "data":[]};
+			for(var i = 0 ; i < results.length ; i++)
+			{
+				output.data.push({"id": results[i].guid, "name": results[i].name, "description": results[i].description,
+						  "isactive": results[i].isactive, "isdeleted": results[i].isdeleted,
+						  "updateddate": results[i].updatedate.replace(" ", "T") });
+			}
+			res.json(output);
 		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
 	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
 };
