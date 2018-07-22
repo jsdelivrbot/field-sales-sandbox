f1d7753cdaddfb8b35cf6bf894de567b7ff50cb7
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-22 10:29:06 +0700

    Update history.js

--- sync/history.js
+++ sync/history.js
@@ -53,7 +53,7 @@ exports.sync = function(req, res, next) {
 					{
 						output.data.push({"id": results[i].guid, "name": results[i].name, "account": results[i].account,
 								  "product": results[i].product, "isdeleted": results[i].isdeleted, 
-								  "updateddate": results[i].updatedate.replace(" ", "T") });
+								  "updateddate": results[i].updatedate.replace(" ", "T") + "+07:00" });
 					}
 					res.json(output);
 				}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
