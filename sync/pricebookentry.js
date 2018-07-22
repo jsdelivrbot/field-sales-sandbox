4aa824bad63a498a871566e295d2660b192b3429
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-22 10:33:57 +0700

    Update pricebookentry.js

--- sync/pricebookentry.js
+++ sync/pricebookentry.js
@@ -37,7 +37,7 @@ exports.sync = function(req, res, next) {
 				output.data.push({"id": results[i].guid, "product": results[i].product__c, 
 						  "pricebook": results[i].price_book__c, "group": results[i].group__c,
 						  "isdeleted": results[i].isdeleted, 
-						  "updateddate": results[i].updatedate.replace(" ", "T")});
+						  "updateddate": results[i].updatedate.replace(" ", "T") + "+07:00"});
 			}
 			res.json(output);
 		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
