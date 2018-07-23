22bd7e0712df44294ee6be154c8fce28bf2210f5
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-23 21:15:51 +0700

    Update productgroup.js

--- sync/productgroup.js
+++ sync/productgroup.js
@@ -58,7 +58,7 @@ exports.sync = function(req, res, next) {
 			for(var i = 0 ; i < results.length ; i++)
 			{
 				output.data.push({"id": results[i].sfid, "name": results[i].name, "columnname": results[i].column_name__c,
-						  "division": results[i].division__c, "parent": results[i].parent__c,
+						  "division": results[i].division__c, "parent": (results[i].parent__c != null ? results[i].parent__c : "root"),
 						  "updatedate": results[i].updatedate.replace(" ", "T") + "+07:00"});
 			}
 			res.json(output);
