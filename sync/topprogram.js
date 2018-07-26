32f41f6944d47408709d9fe960304664c2a0d816
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-07-26 15:33:24 +0700

    Update topprogram.js

--- sync/topprogram.js
+++ sync/topprogram.js
@@ -24,34 +24,39 @@ exports.sync = function(req, res, next) {
 				accountList = accountList.substr(0, accountList.length - 2);
 				accountList += ")";
 				
+				var validData = true;
 				var programlist = "(";
 				for(var i = 0 ; i < req.body.data.length ; i++)
 				{
 					if(req.body.data[i].id != null)
 						programlist += "'" + req.body.data[i].id + "', ";
+					if(req.body.data[i].account == null) validData = false;
 				}
 				programlist = programlist.substr(0, programlist.length - 2);
 				programlist += ")";
 				
-				var query2 = "SELECT guid as Id, name, account__c as account, ";
-				query2 += "to_char( date__c + interval '7 hour', 'YYYY-MM-DD') as date, event_type__c as type, ";
-				//query2 += "success as Success, errorcode as ErrorCode, errormessage as ErrorMessage, ";
-				query2 += "to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updateddate , isdeleted "
-				query2 += "from salesforce.Top_Store_Program__c where (account__c IN " + accountList + " and ";
-				query2 += "systemmodstamp + interval '7 hour' > '" + lastsync2 + "') ";
-				if(req.body.data.length > 0 ) query2 += "or guid IN " + programlist;
-				db.select(query2)
-				.then(function(results2) {
-					for(var i = 0 ; i < results2.length ; i++)
-					{
-						results2[i].updateddate = results2[i].updateddate.replace(" ", "T") + "+07:00";
-					}
-					var output = buildResponse(req.body.data, results2, lastsync, next);
-					output = { "success": true, "errorcode" : "", "errormessage" : "", "data": output };
-					//res.send("Finish!!");
-					console.log(output);
-					res.json(output);
-				}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+				if(validData)
+				{
+					var query2 = "SELECT guid as Id, name, account__c as account, ";
+					query2 += "to_char( date__c + interval '7 hour', 'YYYY-MM-DD') as date, event_type__c as type, ";
+					//query2 += "success as Success, errorcode as ErrorCode, errormessage as ErrorMessage, ";
+					query2 += "to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updateddate , isdeleted "
+					query2 += "from salesforce.Top_Store_Program__c where (account__c IN " + accountList + " and ";
+					query2 += "systemmodstamp + interval '7 hour' > '" + lastsync2 + "') ";
+					if(req.body.data.length > 0 ) query2 += "or guid IN " + programlist;
+					db.select(query2)
+					.then(function(results2) {
+						for(var i = 0 ; i < results2.length ; i++)
+						{
+							results2[i].updateddate = results2[i].updateddate.replace(" ", "T") + "+07:00";
+						}
+						var output = buildResponse(req.body.data, results2, lastsync, next);
+						output = { "success": true, "errorcode" : "", "errormessage" : "", "data": output };
+						//res.send("Finish!!");
+						console.log(output);
+						res.json(output);
+					}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+				} else { res.json({ "success": false, "errorcode" :"10", "errormessage":"Invalid Data" }); }
 			} else { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"No Account" }'); }
 		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
 	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
