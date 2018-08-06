6025a3eec3354aa78d4a561141bc147c67dbc4f2
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-08-06 11:02:39 +0700

    Update accountteam.js

--- server/accountteam.js
+++ server/accountteam.js
@@ -2,6 +2,7 @@ var db = require('./pghelper');
 
 exports.createAccountTeam = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
+	if (req.body.division == null) return res.send('{ \"status\": "fail", \"message\": "No Pricebook" }');
 
 	var query = "INSERT INTO salesforce.account_team__c ( sfid, account__c, salesman__c, division2__c createddate, systemmodstamp, ";
 	query += "IsDeleted ) VALUES ('";
@@ -19,14 +20,19 @@ exports.createAccountTeam = function(req, res, next) {
 exports.createAccountTeamList = function(req, res, next) {
 	if (!req.body) return res.sendStatus(400);
 
+	var haveRecord = false;
 	var query = "INSERT INTO salesforce.account_team__c ( sfid, account__c, salesman__c, division2__c, createddate, systemmodstamp, ";
 	query += "IsDeleted ) VALUES ";
 	for(var i = 0 ; i < req.body.length ; i++)
 	{
-		query += "('" + req.body[i].sfid + "', '" + req.body[i].account + "', '" + req.body[i].sales + "', '" + req.body[i].division;
-		query += "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false), ";
+		if(req.body[i].division)
+		{
+			query += "('" + req.body[i].sfid + "', '" + req.body[i].account + "', '" + req.body[i].sales + "', '" + req.body[i].division;
+			query += "', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false), ";
+			haveRecord = true;
+		}
 	}
-	if(req.body.length > 0 )
+	if(haveRecord == true)
 	{
 		query = query.substr(0, query.length - 2);
 		console.log(query);
