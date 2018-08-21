1bbe6a34dad7f828960b0b887c0bb82746b981f0
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-08-21 11:13:19 +0700

    Update pricebook.js

--- sync/pricebook.js
+++ sync/pricebook.js
@@ -8,37 +8,40 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
-	        query += "FROM salesforce.pricebook2 WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' order by Name asc";
-		db.select(query) 
-		.then(function(results) {
-			/*
-			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
-			for(var i = 0 ; i < results.length ; i++)
-			{
-				output += '{"id":"' + results[i].guid;
-				output += '", "name":"' + results[i].name;
-				output += '", "description":"' + results[i].description;
-				output += '", "isactive":' + results[i].isactive;
-				output += ', "isdeleted":' + results[i].isdeleted;
-				output += ', "updateddate":"' + results[i].updatedate.replace(" ", "T") + '},';
-			}
-			if(results.length)
-			{
-				output = output.substr(0, output.length - 1);
-			}
-			output += ']}';
-			console.log(output);
-			res.json(JSON.parse(output));
-			*/
-			var output = { "success": true, "errorcode" : "", "errormessage" : "", "data":[]};
-			for(var i = 0 ; i < results.length ; i++)
-			{
-				output.data.push({"id": results[i].guid, "name": results[i].name, "description": results[i].description,
-						  "isactive": results[i].isactive, "isdeleted": results[i].isdeleted,
-						  "updateddate": results[i].updatedate.replace(" ", "T") + "+07:00" });
-			}
-			res.json(output);
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+	  	db.init()
+  		.then(function(conn) {
+			var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
+			query += "FROM salesforce.pricebook2 WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' order by Name asc";
+			db.query(query, conn) 
+			.then(function(results) {
+				/*
+				var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					output += '{"id":"' + results[i].guid;
+					output += '", "name":"' + results[i].name;
+					output += '", "description":"' + results[i].description;
+					output += '", "isactive":' + results[i].isactive;
+					output += ', "isdeleted":' + results[i].isdeleted;
+					output += ', "updateddate":"' + results[i].updatedate.replace(" ", "T") + '},';
+				}
+				if(results.length)
+				{
+					output = output.substr(0, output.length - 1);
+				}
+				output += ']}';
+				console.log(output);
+				res.json(JSON.parse(output));
+				*/
+				var output = { "success": true, "errorcode" : "", "errormessage" : "", "data":[]};
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					output.data.push({"id": results[i].guid, "name": results[i].name, "description": results[i].description,
+							  "isactive": results[i].isactive, "isdeleted": results[i].isdeleted,
+							  "updateddate": results[i].updatedate.replace(" ", "T") + "+07:00" });
+				}
+				res.json(output);
+			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
 };
