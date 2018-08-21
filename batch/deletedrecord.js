a5ee64992d7c30b06985660176d40bcdf8e30c41
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-08-21 14:40:43 +0700

    Update deletedrecord.js

--- batch/deletedrecord.js
+++ batch/deletedrecord.js
@@ -1,109 +1,111 @@
 var db = require('../server/pghelper');
+db.init()
+.then(function(conn) {
+  var query = "DELETE FROM salesforce.order_product__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.order_product__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.order WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.order WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.scale_price__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.scale_price__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.pricebook_entry__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.pricebook_entry__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.pricebook2 WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.pricebook2 WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.product2 WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.product2 WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.product_group__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.product_group__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.promotion__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.promotion__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.top_store_program__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.top_store_program__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.product_history__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.product_history__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.good_return__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.good_return__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.call_card__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.call_card__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.call_visit__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.call_visit__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.invoice__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.invoice__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.contact WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.contact WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.account_team__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.account_team__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.account WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.account WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
+  }, function(err) { console.log(err); })
 
-}, function(err) { console.log(err); })
+  var query = "DELETE FROM salesforce.salesman__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
+  db.query(query, conn)
+  .then(function(results) {
 
-var query = "DELETE FROM salesforce.salesman__c WHERE isdeleted = true and systemmodstamp < NOW() - interval '1 months' RETURNING *";
-db.select(query)
-.then(function(results) {
-
+  }, function(err) { console.log(err); })
 }, function(err) { console.log(err); })
