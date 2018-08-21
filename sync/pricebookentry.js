e6c947263393700aaf77c6b46c6a46b5bb4d7aa4
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-08-21 11:14:10 +0700

    Update pricebookentry.js

--- sync/pricebookentry.js
+++ sync/pricebookentry.js
@@ -8,38 +8,41 @@ exports.sync = function(req, res, next) {
   auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
-	        query += "FROM salesforce.pricebook_entry__c WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' ";
-		db.select(query) 
-		.then(function(results) {
-			/*
-			var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
-			for(var i = 0 ; i < results.length ; i++)
-			{
-				output += '{"id":"' + results[i].guid;
-				output += '", "product":"' + results[i].product__c;
-				output += '", "pricebook":"' + results[i].price_book__c;
-				output += '", "group":"' + results[i].group__c;
-				output += '", "isdeleted":' + results[i].isdeleted;
-				output += ', "updateddate":"' + results[i].updatedate.replace(" ", "T") + '"},';
-			}
-			if(results.length)
-			{
-				output = output.substr(0, output.length - 1);
-			}
-			output += ']}';
-			console.log(output);
-			res.json(JSON.parse(output));
-			*/
-			var output = { "success": true, "errorcode" : "", "errormessage" : "", "data":[]};
-			for(var i = 0 ; i < results.length ; i++)
-			{
-				output.data.push({"id": results[i].guid, "product": results[i].product__c, 
-						  "pricebook": results[i].price_book__c, "group": results[i].group__c,
-						  "isdeleted": results[i].isdeleted, 
-						  "updateddate": results[i].updatedate.replace(" ", "T") + "+07:00"});
-			}
-			res.json(output);
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+		db.init()
+	  	.then(function(conn) {
+			var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
+			query += "FROM salesforce.pricebook_entry__c WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' ";
+			db.query(query, conn) 
+			.then(function(results) {
+				/*
+				var output = '{ "success": true, "errorcode" : "", "errormessage" : "", "data":[';
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					output += '{"id":"' + results[i].guid;
+					output += '", "product":"' + results[i].product__c;
+					output += '", "pricebook":"' + results[i].price_book__c;
+					output += '", "group":"' + results[i].group__c;
+					output += '", "isdeleted":' + results[i].isdeleted;
+					output += ', "updateddate":"' + results[i].updatedate.replace(" ", "T") + '"},';
+				}
+				if(results.length)
+				{
+					output = output.substr(0, output.length - 1);
+				}
+				output += ']}';
+				console.log(output);
+				res.json(JSON.parse(output));
+				*/
+				var output = { "success": true, "errorcode" : "", "errormessage" : "", "data":[]};
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					output.data.push({"id": results[i].guid, "product": results[i].product__c, 
+							  "pricebook": results[i].price_book__c, "group": results[i].group__c,
+							  "isdeleted": results[i].isdeleted, 
+							  "updateddate": results[i].updatedate.replace(" ", "T") + "+07:00"});
+				}
+				res.json(output);
+			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
 };
