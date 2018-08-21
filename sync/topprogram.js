cab3f472dd82dcecb66133d96ad0f8a9849166f4
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-08-21 11:20:41 +0700

    Update topprogram.js

--- sync/topprogram.js
+++ sync/topprogram.js
@@ -10,59 +10,62 @@ exports.sync = function(req, res, next) {
 	auth.authen(head)
 	.then(function(obj) {
 		var sales = obj.nickname;
-		var query = "SELECT * FROM salesforce.Account WHERE sfid IN ";
-		query += "(SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales + "')";
-		db.select(query) 
-		.then(function(results) {
-			if(results.length > 0)
-			{
-				var accountList = "(";
-				for(var i = 0 ; i < results.length ; i++)
+		db.init()
+  		.then(function(conn) {
+			var query = "SELECT * FROM salesforce.Account WHERE sfid IN ";
+			query += "(SELECT account__c FROM salesforce.account_team__c WHERE LOWER(salesman__c) = '" + sales + "')";
+			db.query(query, conn) 
+			.then(function(results) {
+				if(results.length > 0)
 				{
-					accountList += "'" + results[i].sfid + "', ";
-				}
-				accountList = accountList.substr(0, accountList.length - 2);
-				accountList += ")";
-				
-				var validData = true;
-				var programlist = "(";
-				for(var i = 0 ; i < req.body.data.length ; i++)
-				{
-					if(req.body.data[i].id != null)
-						programlist += "'" + req.body.data[i].id + "', ";
-					if(req.body.data[i].account == null) validData = false;
-				}
-				programlist = programlist.substr(0, programlist.length - 2);
-				programlist += ")";
-				
-				if(validData)
-				{
-					var query2 = "SELECT guid as Id, name, account__c as account, ";
-					query2 += "to_char( date__c + interval '7 hour', 'YYYY-MM-DD') as date, event_type__c as type, ";
-					//query2 += "success as Success, errorcode as ErrorCode, errormessage as ErrorMessage, ";
-					query2 += "to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updateddate , isdeleted "
-					query2 += "from salesforce.Top_Store_Program__c where (account__c IN " + accountList + " and ";
-					query2 += "systemmodstamp + interval '7 hour' > '" + lastsync2 + "') ";
-					if(req.body.data.length > 0 ) query2 += "or guid IN " + programlist;
-					db.select(query2)
-					.then(function(results2) {
-						for(var i = 0 ; i < results2.length ; i++)
-						{
-							results2[i].updateddate = results2[i].updateddate.replace(" ", "T") + "+07:00";
-						}
-						var output = buildResponse(req.body.data, results2, lastsync, next);
-						output = { "success": true, "errorcode" : "", "errormessage" : "", "data": output };
-						//res.send("Finish!!");
-						console.log(output);
-						res.json(output);
-					}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
-				} else { res.json({ "success": false, "errorcode" :"10", "errormessage":"Invalid Data" }); }
-			} else { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"No Account" }'); }
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+					var accountList = "(";
+					for(var i = 0 ; i < results.length ; i++)
+					{
+						accountList += "'" + results[i].sfid + "', ";
+					}
+					accountList = accountList.substr(0, accountList.length - 2);
+					accountList += ")";
+
+					var validData = true;
+					var programlist = "(";
+					for(var i = 0 ; i < req.body.data.length ; i++)
+					{
+						if(req.body.data[i].id != null)
+							programlist += "'" + req.body.data[i].id + "', ";
+						if(req.body.data[i].account == null) validData = false;
+					}
+					programlist = programlist.substr(0, programlist.length - 2);
+					programlist += ")";
+
+					if(validData)
+					{
+						var query2 = "SELECT guid as Id, name, account__c as account, ";
+						query2 += "to_char( date__c + interval '7 hour', 'YYYY-MM-DD') as date, event_type__c as type, ";
+						//query2 += "success as Success, errorcode as ErrorCode, errormessage as ErrorMessage, ";
+						query2 += "to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updateddate , isdeleted "
+						query2 += "from salesforce.Top_Store_Program__c where (account__c IN " + accountList + " and ";
+						query2 += "systemmodstamp + interval '7 hour' > '" + lastsync2 + "') ";
+						if(req.body.data.length > 0 ) query2 += "or guid IN " + programlist;
+						db.query(query2, conn)
+						.then(function(results2) {
+							for(var i = 0 ; i < results2.length ; i++)
+							{
+								results2[i].updateddate = results2[i].updateddate.replace(" ", "T") + "+07:00";
+							}
+							var output = buildResponse(req.body.data, results2, lastsync, next, conn);
+							output = { "success": true, "errorcode" : "", "errormessage" : "", "data": output };
+							//res.send("Finish!!");
+							console.log(output);
+							res.json(output);
+						}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+					} else { res.json({ "success": false, "errorcode" :"10", "errormessage":"Invalid Data" }); }
+				} else { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"No Account" }'); }
+			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
 };
 
-function buildResponse(update, response, syncdate, next)
+function buildResponse(update, response, syncdate, next, conn)
 {
 	var action = [];
 	for(var j = 0 ; j < update.length ; j++)
@@ -87,11 +90,11 @@ function buildResponse(update, response, syncdate, next)
 		else if(!isInsert) { action.push("update"); }
 		else { action.push("none"); }
 	}
-	syncDB(update, action, next);
+	syncDB(update, action, next, conn);
 	return response;
 };
 
-function syncDB(update, action, next)
+function syncDB(update, action, next, conn)
 {
 	if(update.length > 0)
 	{
@@ -110,11 +113,11 @@ function syncDB(update, action, next)
 			if(update[0].type != null) query += " '" + update[0].type + "',";
 			query += "CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, false, 'Mobile')";
 
-			db.select(query)
+			db.query(query, conn)
 			.then(function(results) {
 				update.shift();
 				action.shift();
-				syncDB(update, action, next);
+				syncDB(update, action, next, conn);
 			})
 			.catch(next);
 		}
@@ -130,11 +133,11 @@ function syncDB(update, action, next)
 			query += "sync_status = 'Mobile' ";
 			query += "WHERE guid = '" + update[0].id + "'";
 
-			db.select(query)
+			db.query(query, conn)
 			.then(function(results) {
 				update.shift();
 				action.shift();
-				syncDB(update, action, next);
+				syncDB(update, action, next, conn);
 			})
 			.catch(next);
 		}	
@@ -142,7 +145,7 @@ function syncDB(update, action, next)
 		{
 			update.shift();
 			action.shift();
-			syncDB(update, action, next);
+			syncDB(update, action, next, conn);
 		}
 	}
 };
