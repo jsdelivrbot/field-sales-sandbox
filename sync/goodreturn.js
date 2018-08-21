469bf5fb11614b653ceb792f237da4beaf3b6551
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-08-21 11:07:06 +0700

    Update goodreturn.js

--- sync/goodreturn.js
+++ sync/goodreturn.js
@@ -51,7 +51,7 @@ exports.sync = function(req, res, next) {
 							if(results2[i].invoice == null) results2[i].invoice = '';
 							results2[i].updatedate = results2[i].updatedate.replace(" ", "T") + "07:00";
 						}
-						var output = buildResponse(req.body.data, results2, lastsync, next);
+						var output = buildResponse(req.body.data, results2, lastsync, next, conn);
 						output = { "success": true, "errorcode" : "", "errormessage" : "", "data": output };
 						//res.send("Finish!!");
 						console.log(output);
