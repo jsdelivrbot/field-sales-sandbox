f3ff3f2cb9d3a14c8076799210fe05d357aa7177
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-09-03 16:44:19 +0700

    Update invoice.js

--- sync/invoice.js
+++ sync/invoice.js
@@ -66,6 +66,7 @@ exports.sync = function(req, res, next) {
 									  "isdeleted": results2[i].isdeleted, 
 									  "updateddate": results2[i].updatedate.replace(" ", "T") + "+07:00"});
 						}
+						console.log('------------------End Invoice----------------');
 						res.json(output);
 					}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
 				} else { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"No Account" }'); }
