7d9b27b1cc65618c78941b425d77fcca8af7d7be
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-09-04 13:21:28 +0700

    Update callvisit.js

--- server/callvisit.js
+++ server/callvisit.js
@@ -173,51 +173,54 @@ exports.getList = function(req, res, next) {
 	var start = req.headers['start'];
 	var startdate = req.headers['start-date'];
 	
-	auth.authen(head)
-	.then(function(obj) {
-		var sales = obj.nickname;
-		var sales = obj.nickname;
-		var query = "SELECT * FROM salesforce.call_visit__c WHERE LOWER(salesman__c) = '" + sales;
-		if(startdate != null)
-		{
-			query += " and createddate > '" + startdate;
-		}
-		query += "' Order by Plan_Start__c asc ";
-		if(!isNaN(limit) && limit > 0)
-		{
-			query += " limit " + limit;
-		}
-		if(!isNaN(start) && start > 0)
-		{
-			query += " OFFSET  " + start;
-		}
-		console.log(query);
-		db.select(query)
-		.then(function(results) {
-			var output = '[';
-			for(var i = 0 ; i < results.length ; i++)
+	db.init()
+  	.then(function(conn) {
+		auth.authen(head, conn)
+		.then(function(obj) {
+			var sales = obj.nickname;
+			var sales = obj.nickname;
+			var query = "SELECT * FROM salesforce.call_visit__c WHERE LOWER(salesman__c) = '" + sales;
+			if(startdate != null)
 			{
-				output += '{"sfid":"' + results[i].sfid;
-				output += '", "Name":"' + results[i].name;
-				output += '", "Account":"' + results[i].account__c;
-				output += '", "Start":"' + results[i].plan_start__c;
-				output += '", "End":"' + results[i].plan_end__c;
-				output += '", "Type":"' + results[i].call_type__c;
-				output += '", "Status":"' + results[i].status__c;
-				output += '", "Comment":"' + results[i].comment;
-				output += '", "IsDeleted":' + results[i].isdeleted;
-				output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
+				query += " and createddate > '" + startdate;
 			}
-			if(results.length)
+			query += "' Order by Plan_Start__c asc ";
+			if(!isNaN(limit) && limit > 0)
 			{
-				output = output.substr(0, output.length - 1);
+				query += " limit " + limit;
 			}
-			output += ']';
-			console.log(output);
-			res.json(JSON.parse(output));
-		}) 
-		.catch(next); 	
-	}, function(err) { res.status(887).send("{ \"status\": \"fail\" }"); })
+			if(!isNaN(start) && start > 0)
+			{
+				query += " OFFSET  " + start;
+			}
+			console.log(query);
+			db.select(query)
+			.then(function(results) {
+				var output = '[';
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					output += '{"sfid":"' + results[i].sfid;
+					output += '", "Name":"' + results[i].name;
+					output += '", "Account":"' + results[i].account__c;
+					output += '", "Start":"' + results[i].plan_start__c;
+					output += '", "End":"' + results[i].plan_end__c;
+					output += '", "Type":"' + results[i].call_type__c;
+					output += '", "Status":"' + results[i].status__c;
+					output += '", "Comment":"' + results[i].comment;
+					output += '", "IsDeleted":' + results[i].isdeleted;
+					output += ', "systemmodstamp":"' + results[i].systemmodstamp + '"},';
+				}
+				if(results.length)
+				{
+					output = output.substr(0, output.length - 1);
+				}
+				output += ']';
+				console.log(output);
+				res.json(JSON.parse(output));
+			}) 
+			.catch(next); 	
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })	
+	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 };
 
 exports.createCallVisit2 = function(req, res, next) {
