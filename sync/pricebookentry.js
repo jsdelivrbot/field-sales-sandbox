baef11d8b6a3a16ddec5be39ea2984c47dcaf0dd
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-09-04 13:46:09 +0700

    Update pricebookentry.js

--- sync/pricebookentry.js
+++ sync/pricebookentry.js
@@ -2,15 +2,15 @@ var db = require('../server/pghelper');
 var auth = require('../server/auth0');
 
 exports.sync = function(req, res, next) {
-  var head = req.headers['authorization'];
-  var lastsync = req.query.syncdate;
-  console.log('------------------Start Pricebook Entry----------------');
+	var head = req.headers['authorization'];
+	var lastsync = req.query.syncdate;
+	console.log('------------------Start Pricebook Entry----------------');
   
-  auth.authen(head)
-	.then(function(obj) {
-		var sales = obj.nickname;
-		db.init()
-	  	.then(function(conn) {
+	db.init()
+	.then(function(conn) {
+		auth.authen(head, conn)
+		.then(function(obj) {
+			var sales = obj.nickname;		
 			var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
 			query += "FROM salesforce.pricebook_entry__c WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' ";
 			db.query(query, conn) 
@@ -45,6 +45,6 @@ exports.sync = function(req, res, next) {
 				console.log('------------------End Pricebook Entry----------------');
 				res.json(output);
 			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
-	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })	
+	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 };
