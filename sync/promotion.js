d6080e043f17e3230d6a6f1e38e35a17593b2f3c
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-09-04 13:49:02 +0700

    Update promotion.js

--- sync/promotion.js
+++ sync/promotion.js
@@ -2,15 +2,15 @@ var db = require('../server/pghelper');
 var auth = require('../server/auth0');
 
 exports.sync = function(req, res, next) {
-  var head = req.headers['authorization'];
-  var lastsync = req.query.syncdate;
-  console.log('------------------Start Promotion----------------');
+	var head = req.headers['authorization'];
+	var lastsync = req.query.syncdate;
+	console.log('------------------Start Promotion----------------');
   
-  auth.authen(head)
-	.then(function(obj) {
-		var sales = obj.nickname;
-	  	db.init()
-  		.then(function(conn) {
+	db.init()
+  	.then(function(conn) {
+		auth.authen(head, conn)
+		.then(function(obj) {
+			var sales = obj.nickname;	  	
 			var query = "SELECT *, to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
 			query += "FROM salesforce.promotion__c WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' ";
 			db.query(query, conn) 
@@ -47,6 +47,6 @@ exports.sync = function(req, res, next) {
 				console.log('------------------End Promotion----------------');
 				res.json(output);
 			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
-	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })	
+	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 };
