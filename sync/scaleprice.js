f1dd8f8bf3cea316accccb76d208144c4c1d2406
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-09-04 13:49:44 +0700

    Update scaleprice.js

--- sync/scaleprice.js
+++ sync/scaleprice.js
@@ -2,15 +2,15 @@ var db = require('../server/pghelper');
 var auth = require('../server/auth0');
 
 exports.sync = function(req, res, next) {
-  var head = req.headers['authorization'];
-  var lastsync = req.query.syncdate;
-  console.log('------------------Start Scale Price----------------');
-  
-  auth.authen(head)
-	.then(function(obj) {
-		var sales = obj.nickname;
-	  	db.init()
-  		.then(function(conn) {
+	var head = req.headers['authorization'];
+	var lastsync = req.query.syncdate;
+	console.log('------------------Start Scale Price----------------');
+
+	db.init()
+  	.then(function(conn) {	
+		auth.authen(head, conn)
+		.then(function(obj) {
+			var sales = obj.nickname;
 			var query = "SELECT *, to_char( systemmodstamp + interval '7 hour', 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
 			query += "FROM salesforce.scale_price__c WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' ";
 			db.query(query, conn) 
@@ -56,6 +56,6 @@ exports.sync = function(req, res, next) {
 				//res.json(JSON.parse(output));
 				//res.send(output);
 			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
-	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })	
+	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 };
