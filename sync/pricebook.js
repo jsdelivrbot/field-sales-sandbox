a51444f34ba007da369b57708c878ddc9eee0996
tholding-app <39939711+tholding-app@users.noreply.github.com>
2018-09-04 13:44:07 +0700

    Update pricebook.js

--- sync/pricebook.js
+++ sync/pricebook.js
@@ -2,15 +2,15 @@ var db = require('../server/pghelper');
 var auth = require('../server/auth0');
 
 exports.sync = function(req, res, next) {
-  var head = req.headers['authorization'];
-  var lastsync = req.query.syncdate;
-  console.log('------------------Start Pricebook----------------');
+  	var head = req.headers['authorization'];
+  	var lastsync = req.query.syncdate;
+  	console.log('------------------Start Pricebook----------------');
   
-  auth.authen(head)
-	.then(function(obj) {
-		var sales = obj.nickname;
-	  	db.init()
-  		.then(function(conn) {
+	db.init()
+	.then(function(conn) {
+	  	auth.authen(head)
+		.then(function(obj) {
+			var sales = obj.nickname;	  	
 			var query = "SELECT *, to_char( systemmodstamp + interval '7 hour' , 'YYYY-MM-DD HH24:MI:SS') as updatedate ";
 			query += "FROM salesforce.pricebook2 WHERE systemmodstamp + interval '7 hour' > '" + lastsync + "' order by Name asc";
 			db.query(query, conn) 
@@ -44,6 +44,6 @@ exports.sync = function(req, res, next) {
 				console.log('------------------End Pricebook----------------');
 				res.json(output);
 			}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"01", "errormessage":"Cannot connect DB." }'); })
-		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
-	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })
+		}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"00", "errormessage":"Authen Fail." }'); })	
+	}, function(err) { res.status(887).send('{ "success": false, "errorcode" :"02", "errormessage":"initial Database fail." }'); })
 };
